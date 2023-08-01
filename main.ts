95a778272a35f1316504c9b7cb7588b61406459f
effectivnayarabota1 <effektivnayarabota1@gmail.com>
2023-08-01 18:31:02 +0300

    application init

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
