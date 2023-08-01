8b77ba5b6f48be9dc459fd98482bff5101e421b5
effectivnayarabota1 <effektivnayarabota1@gmail.com>
2023-08-01 19:39:16 +0300

    template engine connected

--- main.ts
+++ main.ts
@@ -1,11 +1,28 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as path from "https://deno.land/std@0.102.0/path/mod.ts";
+
+import {
+  viewEngine,
+  oakAdapter,
+  etaEngine,
+} from "https://deno.land/x/view_engine@v10.6.0/mod.ts";
+// import { Eta } from "https://deno.land/x/eta@v3.0.3/src/index.ts";
+
+const app = new Application();
+
+app.use(
+  viewEngine(oakAdapter, etaEngine, {
+    viewRoot: "./templates",
+  })
+);
+
+// const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
 const router = new Router();
 router.get("/", (context) => {
-  context.response.body = "HelloWorld";
+  context.render("index.eta");
 });
 
-const app = new Application();
 app.use(router.routes());
 
 await app.listen({ port: 8000 });
