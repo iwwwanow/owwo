2cda8916a0311b2356043dbfba8f50340a1de630
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-08-16 10:02:56 +0300

    module cleaning

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as path from "https://deno.land/std@0.102.0/path/mod.ts";
 
-import GUN from "npm:gun";
-
 import { router } from "./routes/routes.ts";
 
 import {
@@ -21,11 +19,23 @@ await app.use(
 
 await app.use(router.routes());
 
-app.use(async (context) => {
+// static connect
+await app.use(async (context) => {
   await context.send({
     root: `${Deno.cwd()}`,
     // index: "index.html",
   });
 });
+
+const server = await app.addEventListener(
+  "listen",
+  ({ hostname, port, secure }) => {
+    console.log(
+      `Listening on: ${secure ? "https://" : "http://"}${
+        hostname ?? "localhost"
+      }:${port}`
+    );
+  }
+);
 
 await app.listen({ port: 8000 });
