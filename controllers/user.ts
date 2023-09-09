19538ed81f68df993e5f90637dfd2a9c9a523801
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-09-09 12:44:59 +0300

    private settings for profile page

--- controllers/user.ts
+++ controllers/user.ts
@@ -5,39 +5,46 @@ import { eta } from "../config/eta.ts";
 
 export default class user {
   static async index({ request, response, params }) {
+    let editor$, editor, user;
     const username = params.username;
 
-    let editor, user;
-    if (!!request.auth && request.username == username) {
-      editor = request.headers.get("referer").split("/").at(-1);
-
-      user = await e
-        .select(e.User, (user) => ({
-          pages: (page) => ({
-            id: true,
-            state: true,
-            cover: true,
-            title: true,
-            desc: true,
-          }),
-          filter_single: e.op(user.username, "=", username),
-        }))
-        .run(client);
-    } else {
-      user = await e
-        .select(e.User, (user) => ({
-          pages: (page) => ({
-            id: true,
-            state: true,
-            cover: true,
-            title: true,
-            desc: true,
-            filter: e.op(page.state, "=", "default"),
-          }),
-          filter_single: e.op(user.username, "=", username),
-        }))
-        .run(client);
+    interface query {
+      [index: string]: boolean | any;
+      filter?: any;
     }
+
+    let filter: string | boolean = false;
+    function pageQueryFunc(page: object, filter: string | boolean): query {
+      const output: query = {
+        id: true,
+        state: true,
+        cover: true,
+        title: true,
+        desc: true,
+      };
+      if (filter) output.filter = e.op(page.state, "=", filter);
+      console.log(output);
+      return output;
+    }
+
+    // editor = request.headers.get("referer").split("/").at(-1);
+    if (request.auth && request.username === params.username) {
+      // TODO нужно проверить, от автора ли идет этот запрос.
+      // if (request.auth) {
+      //   editor$ = page.authors.some(
+      //     (author) => author.username === request.username
+      //   );
+      // }
+    } else {
+      filter = "default";
+    }
+
+    user = await e
+      .select(e.User, (user) => ({
+        pages: (page) => pageQueryFunc(page, filter),
+        filter_single: e.op(user.username, "=", username),
+      }))
+      .run(client);
 
     response.body = eta.render("./profile", {
       request,
