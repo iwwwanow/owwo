3cc4cac507ccd827464226b00f2d86eca1b3b457
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-09-21 09:54:06 +0300

    eta-init

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import { Elysia } from "elysia";
+import { Eta } from "eta";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8080);
+import * as path from "node:path";
+
+const eta = new Eta({ views: path.join(__dirname, "views") });
+
+const app = new Elysia()
+  .get("/", async () => {
+    return await eta.render("./index", { name: "Ben" });
+  })
+  .listen(8080);
 
 console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
