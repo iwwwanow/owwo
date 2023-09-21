8a67063d7e6cee4833d49f0587dce3c048fafa13
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-09-21 09:31:39 +0300

    elysia init

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
 
+const app = new Elysia().get("/", () => "Hello Elysia").listen(8080);
+
+console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
