4ff5c8dca6e802b8cb000ca7236d14b33eccc8f9
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-09-23 11:37:37 +0300

    index routes done. переключаюсь на регистрацию, авторизацию.

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,45 @@
 import { Elysia } from "elysia";
-import { Database } from "bun:sqlite";
 
-import { eta } from "../config/eta";
+import IndexController from "../controllers";
 
-const db = new Database("data/db.sqlite", { create: true });
+const router = new Elysia();
 
-const router = new Elysia()
-  .get("/", async () => {
-    return eta.render("./index", {});
+// INDEX
+router
+  .get("/", () => {
+    return IndexController.renderIndex();
   })
-
-  .get("/new-user", async ({ set }) => {
-    db.prepare(
-      `
-		CREATE TABLE IF NOT EXISTS users (
-		user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT,
-		password TEXT);
-		`
-    ).run();
-
-    db.prepare(
-      `
-		INSERT INTO users (username, password) VALUES ('admin', 'admin')
-		`
-    ).run();
-
-    set.redirect = "/";
+  .get("/about", () => {
+    return IndexController.renderAbout();
+  })
+  .get("/login", () => {
+    return IndexController.renderLogin();
+  })
+  .get("/signup", () => {
+    return IndexController.renderSignUp();
   });
 
+// AUTH;
+
 export default router;
+
+// import { Database } from "bun:sqlite";
+// const db = new Database("data/db.sqlite", { create: true });
+// .get("/new-user", async ({ set }) => {
+//   db.prepare(
+//     `
+// 		CREATE TABLE IF NOT EXISTS users (
+// 		user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+// 		username TEXT,
+// 		password TEXT);
+// 		`
+//   ).run();
+//
+//   db.prepare(
+//     `
+// 		INSERT INTO users (username, password) VALUES ('admin', 'admin')
+// 		`
+//   ).run();
+//
+//   set.redirect = "/";
+// });
