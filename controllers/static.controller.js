fb282cef18d825cbb26f72b252a3222592698ce0
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-11-08 06:21:17 +0300

    create minimum cache for static 5 min and 1 min for images

--- controllers/static.controller.js
+++ controllers/static.controller.js
@@ -3,16 +3,14 @@ import File from "../middleware/file.middleware";
 export default class Static {
   static send(c) {
     let path;
+    // 5min cache
+    c.headers["Cache-Control"] = "public, max-age=300, must-revalidate";
     if (c.url.pathname === "/favicon.ico") path = "./public/favicon.ico";
     else path = "." + c.url.pathname;
     const file = Bun.file(path);
     if (c.url.pathname.split("/").at(2) === "data_uploads") {
-      // FIX cache for user uploads none!
-      // поставить минимальный кеш, или как-нибудь его обновлять.
-      // console.log(c.url.pathname.split("/").at(2));
-    } else {
-      // 12 hours cache
-      c.headers["Cache-Control"] = "public, max-age=432000, must-revalidate";
+      // 1min cache
+      c.headers["Cache-Control"] = "public, max-age=60, must-revalidate";
     }
     return new Response(file, { headers: c.headers });
   }
