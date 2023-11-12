b3137fb81caf24334df5a9982f2e464abb7327b9
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2023-11-12 13:10:25 +0300

    format all project images

--- src/format.js
+++ src/format.js
@@ -3,95 +3,45 @@ import * as fs from "node:fs";
 import File from "../middleware/file.middleware";
 import Image from "../middleware/image.middleware";
 
-console.log("format started");
-
-const root = `./public/data_uploads/`;
+const uploads = `./public/data_uploads/`;
 
 const getDirectories = async (source) =>
   (await fs.promises.readdir(source, { withFileTypes: true }))
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
-// USERS
-let dirUsers = root + "users/";
-if (fs.existsSync(dirUsers)) {
-  const dirs = await getDirectories(dirUsers);
-  dirs.forEach(async (dir) => {
-    dir = dirUsers + dir + "/";
-    const files = await fs.promises.readdir(dir, { withFileTypes: true });
-    files.forEach(async (file) => {
-      const filename = file.name;
-      const path = dir + filename;
-      if (filename.split("@").length > 1) {
-        fs.rmSync(path);
-      } else {
-        fs.readFile(path, async (error, buffer) => {
-          if (error) {
-            console.error(error.message);
-          }
+const convert = async (path, name, format) => {
+  fs.readFile(path, async (error, buffer) => {
+    if (error) throw new Error(error.message);
 
-          const avatar64 = new Image(buffer);
-          await avatar64.convert("webp64");
+    const path_dir = path.split("/").slice(0, -1).join("/") + "/";
 
-          await File.write(avatar64.buffer, dir, "avatar@webp64.webp");
-        });
-      }
-    });
+    const image = new Image(buffer);
+    await image.convert(format);
+
+    await File.write(image.buffer, path_dir, `${name}@${format}.webp`);
   });
-}
+};
 
-// PAGES
-let dirPages = root + "pages/";
-if (fs.existsSync(dirPages)) {
-  const dirs = await getDirectories(dirPages);
-  dirs.forEach(async (dir) => {
-    dir = dirPages + dir + "/";
-    const files = await fs.promises.readdir(dir, { withFileTypes: true });
-    files.forEach(async (file) => {
-      const filename = file.name;
-      const path = dir + filename;
-      if (filename.split("@").length > 1) {
-        fs.rmSync(path);
-      } else {
-        fs.readFile(path, async (error, buffer) => {
-          if (error) {
-            console.error(error.message);
-          }
+const formatProject = async (root, name, format) => {
+  if (fs.existsSync(root)) {
+    const dirs = await getDirectories(root);
+    dirs.forEach(async (dir) => {
+      const path_dir = root + dir + "/";
+      const files = await fs.promises.readdir(path_dir, {
+        withFileTypes: true,
+      });
 
-          const card = new Image(buffer);
-          await card.convert("webp288");
-
-          await File.write(card.buffer, dir, "cover@webp288.webp");
-        });
-      }
+      files.forEach(async (file) => {
+        const path_file = path_dir + file.name;
+        if (!(await File.removeVariant(path_file))) {
+          await convert(path_file, name, format);
+        }
+      });
     });
-  });
-}
+  }
+};
 
-// ELEMENTS
-let dirElements = root + "elements/";
-if (fs.existsSync(dirElements)) {
-  const dirs = await getDirectories(dirElements);
-  dirs.forEach(async (dir) => {
-    dir = dirElements + dir + "/";
-    const files = await fs.promises.readdir(dir, { withFileTypes: true });
-    files.forEach(async (file) => {
-      const filename = file.name;
-      const path = dir + filename;
-      if (filename.split("@").length > 1) {
-        fs.rmSync(path);
-      } else {
-        fs.readFile(path, async (error, buffer) => {
-          if (error) {
-            console.error(error.message);
-          }
-
-          const card = new Image(buffer);
-          await card.convert("webp288");
-
-          await File.write(card.buffer, dir, "cover@webp288.webp");
-        });
-      }
-    });
-  });
-}
+await formatProject(uploads + "users/", "avatar", "webp64");
+await formatProject(uploads + "pages/", "cover", "webp288");
+await formatProject(uploads + "elements/", "cover", "webp288");
