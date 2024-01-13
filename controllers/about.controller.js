9f2b9ac7239ab4f8dd3291ebf1fa501a10bf1587
effectivnayarabota1 <ivanov.nikulich@gmail.com>
2024-01-13 07:34:22 +0300

    layout template fix

--- controllers/about.controller.js
+++ controllers/about.controller.js
@@ -3,7 +3,6 @@ import render from "../middleware/render.middleware";
 export default class AboutController {
   static async index(c) {
     const html = await render("About", { props: "data" });
-    console.log(html);
     return c.html(html);
   }
 }
