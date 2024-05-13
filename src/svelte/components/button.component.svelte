d6ccee3cea59337597e0f85d786457a84c47d08b
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-13 08:46:46 +0300

    feat: add plus svg to button component

--- src/svelte/components/button.component.svelte
+++ src/svelte/components/button.component.svelte
@@ -1,5 +1,25 @@
 <script>
-	export let style;
+  export let style;
+
+  import ButtonPlusSvg from "./svg/button_plus.svg.svelte";
 </script>
 
-<h5>{style} button</h5>
+{#if style === "plus"}
+  <button class="button button_plus border">
+    <h5>plus button</h5>
+    <ButtonPlusSvg />
+  </button>
+{:else}
+  <h5>other style button</h5>
+{/if}
+
+<style>
+  .button_plus {
+    width: 100%;
+    height: 100%;
+    min-height: 80px;
+  }
+  .button_plus:hover {
+    background-color: var(--dark);
+  }
+</style>
