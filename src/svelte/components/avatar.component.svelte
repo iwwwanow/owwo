2d50e912ca64e3d83d2ec88b6f3f53527fdb7d4a
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-24 06:30:26 +0300

    fix(avatar-component): change avatar type prop to size prop

--- src/svelte/components/avatar.component.svelte
+++ src/svelte/components/avatar.component.svelte
@@ -1,11 +1,11 @@
 <script>
   export let user;
-  export let type;
-  if (!type) type = "blob";
+  export let size;
+  if (!size) size = "blob";
 </script>
 
 <picture>
-  <source srcset="{user.avatar[type]}, {user.avatar[`${type}_2x`]} 2x" />
+  <source srcset="{user.avatar[size]}, {user.avatar[`${size}_2x`]} 2x" />
   <source srcset="https://images.placeholders.dev/?width=64&height=64" />
   <!-- TODO либо сделать разные модификаторы блока, либо разнести по разным файлам -->
   <!-- TODO нужно добавлять дополнительные разрешения для аватаров на хайрезах -->
