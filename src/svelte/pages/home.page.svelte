6003954c0400189ff758590262270f7f53b9c434
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-24 07:19:50 +0300

    feat(home-page): add todos to реклама

--- src/svelte/pages/home.page.svelte
+++ src/svelte/pages/home.page.svelte
@@ -14,6 +14,23 @@
       {/each}
     </span>
   </div>
+
+  <div class="grid todo" style="color: red;">
+    <ul>
+      <li>реклама в пикселях</li>
+      <li>чем ближе пиксель к левому краю, тем выше цена</li>
+      <li>потомучто его увидят большее количество людей</li>
+    </ul>
+  </div>
+
+  <div class="grid todo" style="color: red;">
+    <ul>
+      <li>реклама в блоках</li>
+      <li>дать пользователю сделать адаптив под несколько разрешений</li>
+      <li>сообщить о том, как будет выглядеть на nojs версии</li>
+    </ul>
+  </div>
+
   <div class="grid">
     TODO redirect to effectivnayarabota1 page OWWO
     <h1>owwo big-logo</h1>
