61b655860c7cd9be2fbc38c65c6c896e70d58746
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-24 06:35:53 +0300

    fix(avatar-component): delete comments

--- src/svelte/components/avatar.component.svelte
+++ src/svelte/components/avatar.component.svelte
@@ -6,9 +6,7 @@
 
 <picture>
   <source srcset="{user.avatar[size]}, {user.avatar[`${size}_2x`]} 2x" />
-  <source srcset="https://images.placeholders.dev/?width=64&height=64" />
   <!-- TODO либо сделать разные модификаторы блока, либо разнести по разным файлам -->
-  <!-- TODO нужно добавлять дополнительные разрешения для аватаров на хайрезах -->
   <img
     class="avatar__image"
     src="https://images.placeholders.dev/?width=64&height=64"
