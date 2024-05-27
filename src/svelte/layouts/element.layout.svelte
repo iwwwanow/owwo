e4b1311068301d64a36f2295ec9a458bdfc26724
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-27 11:22:18 +0300

    feat(element.layout): create an element layout

---
+++ src/svelte/layouts/element.layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Head from "../components/head.component.svelte";
+  import Header from "../components/header.component.svelte";
+  import Footer from "../components/footer.component.svelte";
+</script>
+
+<svelte:head>
+  <Head />
+</svelte:head>
+
+<body class="wrapper">
+  <slot />
+  <Header />
+</body>
