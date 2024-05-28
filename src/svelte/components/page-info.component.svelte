14c45945335a2879fca3c678c2d749176a0e83b3
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-28 08:55:13 +0300

    feat(page-info.component): add component; similar to user info

---
+++ src/svelte/components/page-info.component.svelte
@@ -0,0 +1,24 @@
+<script>
+  import PageInfoCover from "./page-info__cover.component.svelte";
+  export let page;
+</script>
+
+<a href="/page/{page.pageId}" class="page-info__container">
+  <PageInfoCover cover={page.cover} />
+  <h5 style="word-break: break-all" class="user-info__username">
+    {page.title}
+  </h5>
+</a>
+
+<style>
+  .page-info__container {
+    --color: var(--dark);
+    display: flex;
+    gap: calc(var(--gap-grid) / 8);
+    text-decoration-color: var(--color);
+    color: var(--color);
+  }
+  .page-info__container:hover {
+    opacity: 0.8;
+  }
+</style>
---
+++ src/svelte/components/page-info__cover.component.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let cover;
+</script>
+
+<picture>
+  <source srcset="{cover.w10}, {cover.w10_2x} 2x" />
+  <source srcset="https://images.placeholders.dev/?width=64&height=64" />
+  <img
+    src="https://images.placeholders.dev/?width=64&height=64"
+    alt="page-cover"
+  />
+</picture>
