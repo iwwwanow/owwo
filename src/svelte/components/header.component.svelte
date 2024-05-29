7fcf697401075688660ab56fce7ed91dfb33bcff
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-29 19:12:03 +0300

    fix(header.component): changle logo to component

--- src/svelte/components/header.component.svelte
+++ src/svelte/components/header.component.svelte
@@ -1,9 +1,9 @@
+<script>
+  import Logo from "./logo.component.svelte";
+</script>
+
 <header class="grid">
-  <h5 class="header__logo-container">
-    <a href="/">
-      <img class="logo" src="/public/assets/logo.svg" alt="logo" />
-    </a>
-  </h5>
+  <Logo />
 
   <h5 class="header__editor-link">editor</h5>
 
@@ -21,20 +21,6 @@
     margin: auto 0 0;
   }
 
-  .header__logo-container {
-    width: fit-content;
-    height: fit-content;
-    min-height: 1em;
-    position: relative;
-  }
-  .header__logo-container a {
-    height: 100%;
-    display: block;
-  }
-  .header__logo-container img {
-    height: 100%;
-  }
-
   .header__login-container {
     grid-column-end: -1;
     grid-row-start: 1;
@@ -49,9 +35,6 @@
   }
 
   @media screen and (max-width: 360px) {
-    .header__logo-container {
-      grid-row: 1/2;
-    }
     .header__login-container {
       grid-row-start: 2;
       justify-content: flex-start;
