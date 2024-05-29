980375567a21dab65b9586039293df8275a6a435
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-29 18:30:21 +0300

    feat(full-grid-wrap-container.component): add component

---
+++ src/svelte/components/full-grid-wrap-container.component.svelte
@@ -0,0 +1,16 @@
+<div class="grid full-grid-wrap-container__wrapper">
+  <span class="full-grid-wrap-container__container">
+    <slot />
+  </span>
+</div>
+
+<style>
+  .full-grid-wrap-container__container {
+    grid-column: 1/-1;
+    position: relative;
+
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+</style>
