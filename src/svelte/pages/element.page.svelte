95b793c6804f8a9b8e345d0da3652d2ab907498b
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-05-30 10:42:23 +0300

    feat(element.page): move content to component

--- src/svelte/pages/element.page.svelte
+++ src/svelte/pages/element.page.svelte
@@ -6,38 +6,17 @@
   import Hr from "../components/hr.component.svelte";
   import PageInfo from "../components/page-info.component.svelte";
   import Card from "../components/card.component.svelte";
-  import ElementInfo from "../components/element-info.component.svelte";
   import FullGridWrapContainer from "../components/full-grid-wrap-container.component.svelte";
   import Logo from "../components/logo.component.svelte";
   import Header from "../components/header.component.svelte";
+  import ElementContent from "../fragments/element__content.fragment.svelte";
+  import ElementInfo from "../components/element-info.component.svelte";
 
   export let element;
 </script>
 
 <ElementLayout>
-  <div class="element__content-wrapper">
-    <div class="element__content-container">
-      <h1>element-content</h1>
-    </div>
-    {#if element.navigationElements}
-      <div class="grid navigation-elements__wrapper">
-        <div class="navigation-elements__container">
-          <span class="navigation-elements__element">
-            <ElementInfo
-              element={element.navigationElements.prevElement}
-              leftSymbol="◂"
-            />
-          </span>
-          <span class="navigation-elements__element">
-            <ElementInfo
-              element={element.navigationElements.nextElement}
-              rightSymbol="▸"
-            />
-          </span>
-        </div>
-      </div>
-    {/if}
-  </div>
+  <ElementContent {element} />
 
   <div class="wrapper element-layout">
     <div class="grid element-info__data-wrapper">
@@ -96,50 +75,10 @@
     color: var(--dark) !important;
   }
 
-  .element__content-wrapper {
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .element__content-container {
-    width: 100%;
-    min-height: calc(100vh - 32px);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
   .element-info__pages-container {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     gap: calc(var(--gap-grid) / 4);
-  }
-
-  .navigation-elements__container * {
-    margin: 0;
-  }
-
-  .navigation-elements__element {
-    width: fit-content;
-    display: flex;
-    flex-direction: row;
-    gap: var(--gap-v);
-  }
-
-  .navigation-elements__wrapper {
-    width: 100%;
-    padding: 8px;
-  }
-
-  .navigation-elements__container {
-    grid-column: 1/-1;
-    display: flex;
-    flex-direction: row;
-    row-gap: var(--gap-v);
-    flex-wrap: wrap-reverse;
-    justify-content: space-between;
   }
 </style>
