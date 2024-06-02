aef498ced74018c5f371e7b663ed38d938a12dad
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-02 08:04:06 +0300

    feat(test-data): add text static data

---
+++ test/test-data/text.test-data.js
@@ -0,0 +1,26 @@
+import { marked } from "marked";
+import DOMPurify from "isomorphic-dompurify";
+
+const testTextPath = "../test-assets/text.test-asset.md";
+const testTextFile = Bun.file(testTextPath);
+const testTextMdString = await testTextFile.text();
+const testTextHtmlString = await marked.parse(testTextMdString);
+const testTextHtmlCleanString = DOMPurify.sanitize(testTextHtmlString);
+
+const makeTextPreview = (text) => {
+  if (text.lenth < 320) return text;
+  // TODO проверь итоговую длину строки? какбудто это неверно
+  else {
+    // const shortenString = text.slice(0, 240);
+    const shortenString = text.slice(0, 240);
+    const wordsArray = shortenString.split(" ");
+    const outputString = wordsArray.slice(0, -1).join(" ") + "...";
+    return outputString;
+  }
+};
+
+export const TEXT_TEST_DATA = {
+  markdown: testTextMdString,
+  html: testTextHtmlCleanString,
+  preview: makeTextPreview(testTextMdString),
+};
