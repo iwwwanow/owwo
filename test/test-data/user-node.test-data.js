a73ed83d755a84d94814a043c435f895f2e110de
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-02 08:05:24 +0300

    feat(test-data): add user-node static data

---
+++ test/test-data/user-node.test-data.js
@@ -0,0 +1,7 @@
+import { IMAGE_TEST_DATA_AVATAR } from "./image.test-data";
+
+export const USER_NODE_TEST_DATA = {
+  id: "testUserId",
+  username: "test-username",
+  avatar: IMAGE_TEST_DATA_AVATAR,
+};
