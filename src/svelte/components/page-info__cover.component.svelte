ed5ec44785cb8ad0f3cd9688a5bb156c6d3e9f33
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-03 08:39:22 +0300

    fix(page-info__cover.component): change w param to h param

--- src/svelte/components/page-info__cover.component.svelte
+++ src/svelte/components/page-info__cover.component.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <picture>
-  <source srcset="{cover.w10}, {cover.w10_2x} 2x" />
+  <source srcset="{cover.h16}, {cover.h16_2x} 2x" />
   <source srcset="https://images.placeholders.dev/?width=64&height=64" />
   <img
     src="https://images.placeholders.dev/?width=64&height=64"
