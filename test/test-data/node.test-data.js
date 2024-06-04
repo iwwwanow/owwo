05acdcfe95a7384b6228046ce9c096e6567d8926
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-04 08:01:43 +0300

    feat(node.test-data): add node test data object
    node will be a common data model for all page types

---
+++ test/test-data/node.test-data.js
@@ -0,0 +1,37 @@
+// 240 symbols long
+// 240lines, TODO test it
+const DESCRIPTION = `
+2PasIihGMQ
+1gltvk2u0A
+8bQhe81dEt
+yT1RlCXyBq
+HAwymDHtCp
+9njapnZiFO
+LmPAggzo0C
+UaIwDpmKzO
+FqdC592HPE
+wvdJ1I0x7v
+nLlXrTD2ab
+bz0yQOhq6y
+lEO0Uw8fbO
+VMcMicE80y
+EFny80tIN8
+x9WSl5eYZ4
+s1dqmClv3P
+cPqgaxiTQU
+41Dbgkwqc4
+ODQeDf17ul
+PysiduEFzP
+85avs4Ym4V
+dJdlVgJ1yG
+3PbogouoCW
+`;
+
+export const NODE_TEST_DATA = {
+  id: "node-id",
+  type: "node-type",
+  // on user-node this is username:
+  title: "node-title",
+  // 240 symbols max, h5 tag on every page
+  description: DESCRIPTION,
+};
