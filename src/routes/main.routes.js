c12cb56237c34012319114656aada6c750c6bbf8
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-05 09:41:50 +0300

    draft(routes): change (user, page, element) routes to single node note

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,15 +1,16 @@
-import {routes} from "@stricjs/app";
-import {StaticController} from "../controllers/static.controller";
-import {ViewController} from "../controllers/view.controller";
+import { routes } from "@stricjs/app";
+import { StaticController } from "../controllers/static.controller";
+import { ViewController } from "../controllers/view.controller";
 
 export default routes()
-	.get("/favicon.ico", () => console.log('TODO create favicon'))
-	.get("/public/*", StaticController.sendFile)
-	.get("/", ViewController.renderHomePage)
-	.get("/about", ViewController.renderAboutPage)
-	.get("/login", ViewController.renderLoginPage)
-	.get("/signup", ViewController.renderSignupPage)
-	.get("/:username", ViewController.renderUserPage)
-	.get("/page/:pageId", ViewController.renderPagePage)
-	.get("/element/:elementId", ViewController.renderElementPage)
-	.get("/error", ViewController.renderErrorPage);
+  .get("/favicon.ico", () => console.log("TODO create favicon"))
+  .get("/public/*", StaticController.sendFile)
+  .get("/", ViewController.renderHomePage)
+  .get("/about", ViewController.renderAboutPage)
+  .get("/login", ViewController.renderLoginPage)
+  .get("/signup", ViewController.renderSignupPage)
+  .get("/:nodeId", ViewController.renderNodePage);
+// .get("/:username", ViewController.renderUserPage)
+// .get("/page/:pageId", ViewController.renderPagePage)
+// .get("/element/:elementId", ViewController.renderElementPage)
+// .get("/error", ViewController.renderErrorPage);
