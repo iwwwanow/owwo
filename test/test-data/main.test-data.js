e3f7c055877dfefc8975cd035393f771034a6567
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-07 12:04:16 +0300

    feat(test-data): create main data

---
+++ test/test-data/main.test-data.js
@@ -0,0 +1,18 @@
+// import { TEXT_CONTENT_TEST_DATA as DESCRIPTION_TEST_DATA } from "./text.test-data";
+import { DESCRIPTION_TEST_DATA } from "./text.test-data";
+
+// 240 symbols long
+// 240lines, TODO test it
+// const DESCRIPTION
+
+export const MAIN_TEST_DATA = {
+  //user page element
+  type: "node-type",
+  // on user-node this is username:
+  // 80 symbols max? channelOfTheCultOfTheGoddessOfFlowers
+  title: "node-title",
+  // 240 symbols max, h5 tag on every page
+  // description: DESCRIPTION,
+  description: DESCRIPTION_TEST_DATA.html,
+  blob: "https://images.placeholders.dev/?width=8&height=8",
+};
