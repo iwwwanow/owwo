375158648bd655c3400e582f2d9a1b96552ee48d
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-17 22:04:31 +0300

    fix(test-data): id only

--- test/test-data/meta.test-data.js
+++ test/test-data/meta.test-data.js
@@ -2,7 +2,7 @@ const NODE_ID = "node-id";
 
 export const META_TEST_DATA = {
   id: NODE_ID,
-  parents: [NODE_ID, NODE_ID, NODE_ID],
-  childs: [NODE_ID, NODE_ID, NODE_ID],
-  siblings: [NODE_ID, NODE_ID, NODE_ID],
+  // parents: [NODE_ID, NODE_ID, NODE_ID],
+  // childs: [NODE_ID, NODE_ID, NODE_ID],
+  // siblings: [NODE_ID, NODE_ID, NODE_ID],
 };
