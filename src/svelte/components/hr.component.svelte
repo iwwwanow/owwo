e4fb49cc44729734652034dde12681d2138a5a45
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-18 13:18:26 +0300

    feat(hr): add text prop

--- src/svelte/components/hr.component.svelte
+++ src/svelte/components/hr.component.svelte
@@ -1,9 +1,23 @@
-<hr />
+<script>
+  export let text;
+</script>
+
+{#if text}
+  <fieldset><legend><h6>{text}</h6></legend></fieldset>
+{:else}
+  <hr />
+{/if}
 
 <style>
-  hr {
+  hr,
+  fieldset {
     grid-column: 1/ -1;
+    padding-left: 8px;
     border-top: 1px solid var(--light);
-    margin: 12px 0;
+    color: var(--medium);
+    margin: 12px 0 4px;
+  }
+  legend {
+    padding: 0 4px;
   }
 </style>
