f85328e2201a60af5741819e6e5bc6e20ddb5791
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-19 11:21:22 +0300

    fix(extended.fragment): change padding to gap

--- src/svelte/layouts/node-extended.layout.svelte
+++ src/svelte/layouts/node-extended.layout.svelte
@@ -6,6 +6,14 @@
   <Head />
 </svelte:head>
 
-<body>
+<body class="node-extended-layout">
   <slot />
 </body>
+
+<style>
+  .node-extended-layout {
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+  }
+</style>
