f3aceb5981df6da132607bd4a0e84ca3c98e44d1
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-30 18:26:25 +0300

    feat(login-page): create page without buttons

--- src/svelte/pages/login.page.svelte
+++ src/svelte/pages/login.page.svelte
@@ -1,7 +1,24 @@
 <script>
   import BaseLayout from "../layouts/base.layout.svelte";
+  import TextInput from "../components/inputs/text.input.svelte";
+
+  const LOGIN_PLACEHOLDER = "username".toUpperCase();
+  const PASSWORD_PLACEHOLDER = "password".toUpperCase();
 </script>
 
 <BaseLayout>
-  <div class="grid">login</div>
+  <div class="grid">
+    <form method="POST" action="/login" class="login-form">
+      <TextInput
+        name="username"
+        required={true}
+        placeholder={LOGIN_PLACEHOLDER}
+      />
+      <TextInput
+        name="password"
+        required={true}
+        placeholder={PASSWORD_PLACEHOLDER}
+      />
+    </form>
+  </div>
 </BaseLayout>
