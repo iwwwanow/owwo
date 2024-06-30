18e220479bf035b2ccebc92c504b97859e7f4535
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-06-30 18:25:59 +0300

    feat(inputs): create text-input component

---
+++ src/svelte/components/inputs/text.input.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let id;
+  export let name;
+  export let placeholder = "";
+  export let required = false;
+</script>
+
+<p class="text-input">
+  <input
+    class="text-input__input"
+    autocomplete="off"
+    autocapitalize="off"
+    {required}
+    {id}
+    {name}
+    {placeholder}
+    type="text"
+  />
+</p>
+
+<style>
+  .text-input__input {
+    width: 100%;
+    padding: 8px 4px;
+
+    color: var(--DARK);
+    background-color: var(--LIGHT);
+
+    font-style: inherit;
+    font-weight: inherit;
+    text-transform: inherit;
+  }
+
+  .text-input__input:focus {
+    color: var(--black);
+    background-color: var(--WHITE);
+    outline: 1px solid var(--BLACK);
+    border: none;
+  }
+
+  .text-input__input::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: var(--MEDIUM);
+    opacity: 1;
+    /* Firefox */
+  }
+
+  .text-input__input:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: var(--MEDIUM);
+  }
+
+  .text-input__input::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: var(--MEDIUM);
+  }
+  @media screen and (max-width: 444px) {
+    .text-input__input {
+      border-radius: 4px;
+    }
+  }
+</style>
