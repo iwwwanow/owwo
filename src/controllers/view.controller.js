d1a80769a6e682b30ec3a74d1f22c65bc3e08910
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-01 19:50:57 +0300

    refactor(view-controller): clean unused imports and methods

--- src/controllers/view.controller.js
+++ src/controllers/view.controller.js
@@ -2,22 +2,17 @@ import { html } from "@stricjs/app/send";
 import { SveltePageView } from "../views/svelte-page.view.js";
 
 import { NodeModel } from "../models/node.model.js";
-import { DataTestModel } from "../../test/test-models/data.test-model.js";
-import { getTextFileContentHelper } from "../helpers/get-text-file-content.helper.js";
-import { convertMdHtmlHelper } from "../helpers/convert-md-html.helper.js";
 
 import HomePage from "../svelte/pages/home.page.svelte";
 import AboutPage from "../svelte/pages/about.page.svelte";
 import LoginPage from "../svelte/pages/login.page.svelte";
 import SignupPage from "../svelte/pages/signup.page.svelte";
-import ErrorPage from "../svelte/pages/error.page.svelte";
 
 import NodePage from "../svelte/pages/node.page.svelte";
 import NodeExtendedPage from "../svelte/pages/node-extended.page.svelte";
 
-// import User from "../svelte/pages/User.page.svelte";
-// import Page from "../svelte/pages/Page.page.svelte";
-// import Element from "../svelte/pages/Element.page.svelte";
+import { getTextFileContentHelper } from "../helpers/get-text-file-content.helper.js";
+import { convertMdHtmlHelper } from "../helpers/convert-md-html.helper.js";
 
 export class ViewController {
   static async responsePageHtml(componentName, props) {
@@ -60,62 +55,6 @@ export class ViewController {
       return ViewController.responsePageHtml(NodeExtendedPage, props);
     }
   }
-
-  // static async renderNodePage({ params }) {
-  //   const { nodeId } = params;
-  //   if (nodeId === "username") {
-  //     const userNodeData = await DataTestModel.getUserNodeData();
-  //     const pageNodeData = await DataTestModel.getPageNodeData();
-  //
-  //     // TODO change childs to page node
-  //     userNodeData.childs = [pageNodeData, pageNodeData];
-  //
-  //     const props = { node: userNodeData };
-  //     return ViewController.responsePageHtml(NodePage, props);
-  //   } else if (nodeId === "page-id") {
-  //     const pageNodeData = await DataTestModel.getPageNodeData();
-  //
-  //     // TODO change childs to page node
-  //     pageNodeData.childs = [pageNodeData, pageNodeData];
-  //
-  //     const userNodeData = await DataTestModel.getUserNodeData();
-  //     pageNodeData.authors = [userNodeData, userNodeData];
-  //
-  //     const props = { node: pageNodeData };
-  //     return ViewController.responsePageHtml(NodePage, props);
-  //   }
-  // }
-
-  // static async renderNodePage({ params }) {
-  //   const { nodeId } = params;
-  //   console.log(nodeId);
-  //   const nodeData = await DataTestModel.getNodeData();
-  //   nodeData.childs = [nodeData, nodeData];
-  //   const props = { node: nodeData };
-  //   return ViewController.responsePageHtml(NodePage, props);
-  // }
-
-  // static async renderUserPage() {
-  //   const user = await DataTestModel.getUserNodeData();
-  //   const props = { user };
-  //   return ViewController.responsePageHtml(User, props);
-  // }
-  //
-  // static async renderPagePage() {
-  //   const page = await DataTestModel.getPageNodeData();
-  //   const props = { page };
-  //   return ViewController.responsePageHtml(Page, props);
-  // }
-  //
-  // static async renderElementPage() {
-  //   const props = {
-  //     element: testElementData,
-  //     randomUserElements: DataTestModel.getElementNodesData(32),
-  //     // TODO maybe make it bigger?
-  //     randomOwwoElements: DataTestModel.getElementNodesData(128),
-  //   };
-  //   return ViewController.responsePageHtml(Element, props);
-  // }
 
   static async renderErrorPage() {
     return ViewController.responsePageHtml(Error);
