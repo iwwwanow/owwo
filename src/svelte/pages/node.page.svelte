bb7c6026daa5e748bf9005a5b234ca94f8d8c8f9
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-04 04:51:03 +0300

    feat(node.page): render input on add element form (near plus button)

--- src/svelte/pages/node.page.svelte
+++ src/svelte/pages/node.page.svelte
@@ -1,16 +1,19 @@
 <script>
   import BaseLayout from "../layouts/base.layout.svelte";
 
+  import EditorForm from "../components/editor-form.component.svelte";
+  import NodeInfo from "../components/node-info.component.svelte";
+  import NodeCard from "../components/node-card.component.svelte";
+
   import Image from "../components/image.component.svelte";
   import Text from "../components/text.component.svelte";
   import Date from "../components/date.component.svelte";
   import PlusButton from "../components/buttons/plus.button.svelte";
+  import TextInput from "../components/inputs/text.input.svelte";
   import Hr from "../components/hr.component.svelte";
 
-  import NodeInfo from "../components/node-info.component.svelte";
-  import NodeCard from "../components/node-card.component.svelte";
-
   export let node;
+  export let client;
 
   const { id } = node;
   const { image } = node;
@@ -20,9 +23,14 @@
   const { meta } = node;
   const childs = meta.childs;
   const authors = meta.authors;
+
+  const ADD_NODE_INPUT_PLACEHOLDER =
+    "N-(url/id) N-(url/id) N-(url/id)".toUpperCase();
 </script>
 
 <BaseLayout>
+  <EditorForm {client} />
+
   <div class="grid node-wrapper">
     <NodeInfo {node} />
 
@@ -45,6 +53,35 @@
 
   <!-- TODO страницы, в которые пушил User -->
 
+  <div class="grid">
+    <Hr text="node-files"></Hr>
+  </div>
+
+  <div class="grid user__pages-container">
+    {#if childs}
+      {#each childs as childNode}
+        <NodeCard node={childNode} />
+      {/each}
+    {/if}
+
+    <span class="add-node__container">
+      <div class="add-node__input-container">
+        <TextInput
+          id="add-node__input"
+          name="node-data"
+          required={true}
+          placeholder={ADD_NODE_INPUT_PLACEHOLDER}
+        />
+        <PlusButton variant="small" />
+      </div>
+      <PlusButton />
+    </span>
+  </div>
+
+  <div class="grid">
+    <Hr text="child-nodes"></Hr>
+  </div>
+
   <div class="grid user__pages-container">
     {#if childs}
       {#each childs as childNode}
@@ -55,3 +92,20 @@
     <PlusButton />
   </div>
 </BaseLayout>
+
+<style>
+  .add-node__container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: var(--GRID-GAP-2);
+  }
+  :global(.add-node__container *) {
+    margin: 0;
+  }
+  .add-node__input-container {
+    display: flex;
+    flex-direction: row;
+    gap: var(--GRID-GAP-4);
+  }
+</style>
