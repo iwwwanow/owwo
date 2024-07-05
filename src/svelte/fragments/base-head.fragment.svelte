34ec8cfeb3dd6e8a255ef19e057c86b86215be0b
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-05 23:38:14 +0300

    fix(head.component): change OWWO to owwo title

--- src/svelte/fragments/base-head.fragment.svelte
+++ src/svelte/fragments/base-head.fragment.svelte
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <meta name="theme-color" content="#e0e0e0" />
-<title>OWWO</title>
+<title>owwo</title>
 
 <link rel="stylesheet" href="/public/styles/index.css" />
