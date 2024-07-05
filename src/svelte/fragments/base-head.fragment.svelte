334c11056a157939b75cc4784b3c831bd5156e22
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-05 21:52:30 +0300

    refactor(public): change imports to index import

--- src/svelte/fragments/base-head.fragment.svelte
+++ src/svelte/fragments/base-head.fragment.svelte
@@ -4,12 +4,4 @@
 <meta name="theme-color" content="#e0e0e0" />
 <title>OWWO</title>
 
-<link rel="stylesheet" href="/public/styles/reset.css" />
-<link rel="stylesheet" href="/public/styles/scrollbar.css" />
-<link rel="stylesheet" href="/public/styles/hr.css" />
-<link rel="stylesheet" href="/public/styles/border.css" />
-<link rel="stylesheet" href="/public/styles/font.css" />
-
-<link rel="stylesheet" href="/public/styles/layout.css" />
-
-<link rel="stylesheet" href="/public/styles/constants/color.constants.css" />
+<link rel="stylesheet" href="/public/styles/index.css" />
