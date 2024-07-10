a6741c9f16cf0589e65a5993c923eed6af55980a
effectivnayarabota1 <effectivnayarabota1@gmail.com>
2024-07-10 20:16:49 +0300

    refactor(editor-form): move fieldsets to individual components

--- src/svelte/components/editor-form.component.svelte
+++ src/svelte/components/editor-form.component.svelte
@@ -1,9 +1,9 @@
 <script>
   import Hr from "./hr.component.svelte";
-  import TextInput from "./inputs/text.input.svelte";
-  import FileInput from "./inputs/file.input.svelte";
-  import Textarea from "./inputs/textarea.component.svelte";
-  import CardContainer from "./card-container.component.svelte";
+
+  import EditorFormFileInputFieldset from "./editor-form__file-input-fieldset.component.svelte";
+  import EditorFormContentTextareaFieldset from "./editor-form__content-textarea-fieldset.component.svelte";
+  import EditorFormMainInputsFieldset from "./editor-form__main-inputs-fieldset.component.svelte";
 
   export let node;
   const {
@@ -12,58 +12,14 @@
 
   export let client;
   const { isEditor } = client;
-
-  const TITLE_INPUT_PLACEHOLDER = "title".toUpperCase();
-  const DESCRIPTION_TEXTAREA_PLACEHOLDER = "description".toUpperCase();
-  const CONTENT_TEXTAREA_PLACEHOLDER = "content".toUpperCase();
 </script>
 
 <div class="grid editor-form">
   <Hr text="editor-form" color="var(--ORANGE)" />
 
-  <fieldset class="main-inputs-fieldset">
-    <Hr text="cover-input" />
-    <CardContainer>
-      <FileInput name="cover-input" {imageSrc} />
-
-      <TextInput
-        id="editor-form__title-input"
-        name="title"
-        rows="auto"
-        required={false}
-        placeholder={TITLE_INPUT_PLACEHOLDER}
-      />
-
-      <Textarea
-        id="editor-form__description-textarea"
-        name="description"
-        rows="auto"
-        required={false}
-        placeholder={DESCRIPTION_TEXTAREA_PLACEHOLDER}
-        text="240 limit???"
-      />
-    </CardContainer>
-  </fieldset>
-
-  <fieldset class="file-input-fieldset">
-    <Hr text="file-input" />
-    <CardContainer>
-      <FileInput />
-    </CardContainer>
-  </fieldset>
-
-  <fieldset class="grid__right-content">
-    <Hr text="content-textarea" />
-    <CardContainer>
-      <Textarea
-        id="editor-form__content-textarea"
-        name="content"
-        required={false}
-        placeholder={CONTENT_TEXTAREA_PLACEHOLDER}
-        text="no limit, markdown, width like an text input, position too"
-      />
-    </CardContainer>
-  </fieldset>
+  <EditorFormMainInputsFieldset {imageSrc} />
+  <EditorFormFileInputFieldset />
+  <EditorFormContentTextareaFieldset />
 
   <Hr text="***" color="var(--ORANGE)" />
 </div>
@@ -83,40 +39,3 @@
     ссылка (например на другой файл)
   </h5>
 </div>
-
-<style>
-  .file-input-fieldset {
-    grid-column: 2/4;
-  }
-
-  @media screen and (max-width: 1680px) {
-    .file-input-fieldset {
-      grid-column: 2/3;
-    }
-  }
-
-  @media screen and (max-width: 1268px) {
-    .file-input-fieldset {
-      grid-column: 1/-1;
-      grid-row: 2/3;
-    }
-  }
-
-  @media screen and (max-width: 650px) {
-    .main-inputs-fieldset {
-      grid-column: 1/2;
-    }
-    .file-input-fieldset {
-      grid-column: 2/-1;
-    }
-  }
-
-  @media screen and (max-width: 360px) {
-    .main-inputs-fieldset {
-      grid-column: 1/-1;
-    }
-    .file-input-fieldset {
-      grid-column: 1/-1;
-    }
-  }
-</style>
---
+++ src/svelte/components/editor-form__file-input-fieldset.component.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Hr from "./hr.component.svelte";
+  import CardContainer from "./card-container.component.svelte";
+  import FileInput from "./inputs/file.input.svelte";
+</script>
+
+<fieldset class="file-input-fieldset">
+  <Hr text="file-input" />
+  <CardContainer>
+    <FileInput />
+  </CardContainer>
+</fieldset>
+
+<style>
+  .file-input-fieldset {
+    grid-column: 2/4;
+  }
+
+  @media screen and (max-width: 1680px) {
+    .file-input-fieldset {
+      grid-column: 2/3;
+    }
+  }
+
+  @media screen and (max-width: 1268px) {
+    .file-input-fieldset {
+      grid-column: 1/-1;
+      grid-row: 2/3;
+    }
+  }
+
+  @media screen and (max-width: 650px) {
+    .file-input-fieldset {
+      grid-column: 2/-1;
+    }
+  }
+
+  @media screen and (max-width: 360px) {
+    .file-input-fieldset {
+      grid-column: 1/-1;
+    }
+  }
+</style>
