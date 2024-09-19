3ae9c2d46cebf42b177df012d8b3f4e4c860cc1e
Kirill Ivanov <effectivnayarabota1@gmail.com>
2024-09-19 07:12:16 +0300

    feat(plugins): import plugin from bte

--- src/plugins/svelte-runtime.plugin.ts
+++ src/plugins/svelte-runtime.plugin.ts
@@ -1,24 +1,4 @@
 import { plugin } from "bun";
+import { btePlugin } from "bun-template-engine";
 
-await plugin({
-  name: "svelteRuntimePlugin",
-  async setup(build) {
-    const { compile } = await import("svelte/compiler");
-
-    build.onLoad({ filter: /\.svelte$/ }, async ({ path }) => {
-      const file = await Bun.file(path).text();
-
-      const result = compile(file, {
-        filename: path,
-        generate: "ssr",
-      });
-
-      const jsString = result.js.code;
-
-      return {
-        contents: jsString,
-        loader: "js",
-      };
-    });
-  },
-});
+await plugin(btePlugin());
