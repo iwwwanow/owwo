7362cc0b891d562c0d9042cbd0bb84339b3e2a0c
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-24 08:29:34 +0300

    refactor(site/svelte): mv to `svelte-templates` & make package

---
+++ site/svelte-templates/src/pages/index.ts
@@ -0,0 +1,7 @@
+export * as AboutPage from "./about.page.svelte";
+export * as ErrorPage from "./error.page.svelte";
+export * as HomePage from "./home.page.svelte";
+export * as LoginPage from "./login.page.svelte";
+export * as NodeExtendedPage from "./node-extended.page.svelte";
+export * as NodePage from "./node.page.svelte";
+export * as SignupPage from "./signup.page.svelte";
