a42004f84553bd9f6fbbe526028f3fcb206081d1
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-09-25 07:41:42 +0300

    refactor(site/svelte-templates): change impornames

--- site/svelte-templates/src/pages/index.ts
+++ site/svelte-templates/src/pages/index.ts
@@ -1,7 +1,7 @@
-export * as AboutPage from "./about.page.svelte";
-export * as ErrorPage from "./error.page.svelte";
-export * as HomePage from "./home.page.svelte";
-export * as LoginPage from "./login.page.svelte";
-export * as NodeExtendedPage from "./node-extended.page.svelte";
-export * as NodePage from "./node.page.svelte";
-export * as SignupPage from "./signup.page.svelte";
+export { default as AboutPage } from "./about.page.svelte";
+export { default as ErrorPage } from "./error.page.svelte";
+export { default as HomePage } from "./home.page.svelte";
+export { default as LoginPage } from "./login.page.svelte";
+export { default as NodeExtendedPage } from "./node-extended.page.svelte";
+export { default as NodePage } from "./node.page.svelte";
+export { default as SignupPage } from "./signup.page.svelte";
