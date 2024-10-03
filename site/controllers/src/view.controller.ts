5e82118763809a1d66f96c68403effa78cb47d95
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-03 07:35:11 +0300

    refactor(view.controller): move test data to `MockDataModel`

--- site/controllers/src/view.controller.ts
+++ site/controllers/src/view.controller.ts
@@ -1,7 +1,7 @@
 import { getTextFileContentHelper } from "@site/helpers";
 import { convertMdHtmlHelper } from "@site/helpers";
 import { NodeModel } from "@site/models";
-import { NodeTestModel } from "@site/models";
+import { MockModel } from "@site/models";
 import { ClientModel } from "@site/models";
 import { HomePage } from "@site/svelte-templates";
 import { AboutPage } from "@site/svelte-templates";
@@ -30,10 +30,12 @@ export class ViewController {
     // const users = await NodeModel.getNodes("user", 32);
     const TEST_USERS_QUANTITY = 128;
     const nodeId = process.env["TEST_NODE_USERNAME"];
-    const node = new NodeTestModel(nodeId);
+    const node = new NodeTestModel({ nodeId });
     await node.initData();
     const users = Array(TEST_USERS_QUANTITY).fill(node.data);
+
     const props = { users };
+
     return ViewController.responsePageHtml(HomePage, props);
   }
 
@@ -74,8 +76,9 @@ export class ViewController {
       process.env.NODE_ENV === "developement" &&
       testNodeIds.includes(nodeId)
     ) {
-      // const node = new NodeTestModel(nodeId);
-      //
+      nodeData = await MockModel.getUserNodeData();
+
+      // const node = new NodeTestModel({ nodeId });
       // await node.initData();
       // nodeData = node.data;
 
@@ -84,26 +87,23 @@ export class ViewController {
       // можно попробовать продублировать свойство
 
       if (nodeId === TEST_NODE_PAGE_ID) {
-        // const node_user = new NodeTestModel(TEST_NODE_USERNAME);
-        const node_user = new NodeTestModel({ nodeId: TEST_NODE_USERNAME });
+        // const node_user = new NodeTestModel({ nodeId: TEST_NODE_USERNAME });
         // await node_user.initData();
         // nodeData.meta.authors = Array(3).fill(node_user.data);
-
-        console.log(node_user.data.meta.id);
-        const node_parent = new NodeTestModel({ nodeId: TEST_NODE_PAGE_ID });
+        //
+        // const node_parent = new NodeTestModel({ nodeId: TEST_NODE_PAGE_ID });
         // await node_parent.initData();
         // nodeData.meta.parents = Array(8).fill(node_parent.data);
-
-        console.log(node_user.nodeId);
-        console.log(node_parent.nodeId);
       }
 
-      // const node_child = new NodeTestModel(TEST_NODE_PAGE_ID);
+      // const node_child = new NodeTestModel({ nodeId: TEST_NODE_PAGE_ID });
       // await node_child.initData();
       // nodeData.meta.childs = Array(8).fill(node_child.data);
     } else {
       nodeData = await NodeModel.get(nodeId);
     }
+
+    // console.log(nodeData.meta.authors);
 
     // const EDITOR_URL = new URL("", req.url);
     // EDITOR_URL.searchParams.set("editor", true);
--- site/models/src/index.ts
+++ site/models/src/index.ts
@@ -1,3 +1,3 @@
 export * from "./node.model";
-export * from "./node.test.model";
 export * from "./client.model";
+export * from "./mock.model";
