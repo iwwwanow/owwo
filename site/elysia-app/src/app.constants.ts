9a291a7d6ba5c9c00d88d78cc340c04828b32556
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-09 07:24:03 +0300

    refactor(elysia-app): add constants; namings

--- site/elysia-app/src/app.constants.ts
+++
@@ -1,6 +0,0 @@
-// TODO func to utils; text leave here
-
-export const getLaunchText = (url: URL) => {
-  const ELYSIA_APP_LAUNCH_TEXT = `elysia app launched at:`;
-  return `${ELYSIA_APP_LAUNCH_TEXT.toUpperCase()} ${url.href}`;
-};
--- site/elysia-app/src/elysia-app.constants.ts
+++ site/elysia-app/src/elysia-app.constants.ts
@@ -1,1 +1,7 @@
 export const LISTEN_PORT = 3000;
+export const ELYSIA_APP_LAUNCH_TEXT = `elysia app launched at:`;
+export const REQUIRED_ENV_NAMES = [
+  "TEST_NODE_USERNAME",
+  "TEST_NODE_PAGE_ID",
+  "TEST_NODE_ELEMENT_ID",
+];
