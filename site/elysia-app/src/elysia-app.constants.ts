73d337f18bc9d3c3a5e22a876b7433d3add6f3dc
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-10 07:15:06 +0300

    refactor: change node extended page id name

--- site/elysia-app/src/elysia-app.constants.ts
+++ site/elysia-app/src/elysia-app.constants.ts
@@ -3,5 +3,5 @@ export const ELYSIA_APP_LAUNCH_TEXT = `elysia app launched at:`;
 export const REQUIRED_ENV_NAMES = [
   "TEST_NODE_USERNAME",
   "TEST_NODE_PAGE_ID",
-  "TEST_NODE_ELEMENT_ID",
+  "TEST_NODE_EXTENDED_PAGE_ID",
 ];
