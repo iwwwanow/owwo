87dbb569bfccb5d89d98f244a80a561ac5f91890
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-12 10:17:26 +0300

    refactor: rm unused

--- test/mock/src/mock-data/_element-node.test-data.js
+++
@@ -1,8 +0,0 @@
-import { IMAGE_TEST_DATA_COVER } from "./image.test-data";
-
-export const ELEMENT_NODE_TEST_DATA = {
-  elementId: "test-element-id",
-  title: "testElementTitle",
-  // content?
-  cover: IMAGE_TEST_DATA_COVER,
-};
