a73ca542c40364639fa6ef92c6feea3b860e8997
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-13 11:28:03 +0300

    refactor(bte): split rewriter into smaller parts(files)

---
+++ packages/bun-template-engine/src/rewriter/src/append-head.rewriter.ts
@@ -0,0 +1,16 @@
+class AppendHeadRewriter
+  implements HTMLRewriterTypes.HTMLRewriterElementContentHandlers
+{
+  appendContent: string;
+
+  constructor(appendContent: string) {
+    this.appendContent = appendContent;
+  }
+
+  element(element) {
+    // TODO замена заголовка, если он есть!
+    element.append(this.appendContent, { html: true });
+  }
+}
+
+export { AppendHeadRewriter };
--- packages/bun-template-engine/src/rewriter/src/rewriter.ts
+++ packages/bun-template-engine/src/rewriter/src/rewriter.ts
@@ -1,38 +1,17 @@
-import type { ComponentType } from "svelte";
 import { defaultBlankHtml } from "test-templates";
 
-class AppendHeadRewriter {
-  constructor(appendContent) {
-    this.appendContent = appendContent;
-  }
-  element(element) {
-    // TODO замена заголовка, если он есть!
-    element.append(this.appendContent, { html: true });
-  }
-}
-
-class AppendBodyRewriter {
-  constructor(appendContent) {
-    this.appendContent = appendContent;
-  }
-  element(element) {
-    element.append(this.appendContent, { html: true });
-  }
-}
-
-class AppendCssRewriter {
-  constructor(appendContent) {
-    this.appendContent = appendContent;
-  }
-  element(element) {
-    this.appendContent = `<style>${this.appendContent}</style>`;
-    element.append(this.appendContent, { html: true });
-  }
-}
+import { AppendBodyRewriter } from "./append-body.rewriter";
+import { AppendCssRewriter } from "./append-css.rewriter";
+import { AppendHeadRewriter } from "./append-head.rewriter";
+import type { SvelteComponentType } from "./rewriter.interfaces";
 
 export class Bte {
   private blankHtmlPath: string;
   blankHtmlPage?: string;
+
+  head?: string;
+  body?: string;
+  css?: string;
 
   constructor(blankHtmlPath?: string) {
     // TODO можно импортировать сразу html-страницу,
@@ -48,17 +27,19 @@ export class Bte {
     this.blankHtmlPage = blankHtmlString;
   }
 
-  async getPageHtml(svelteComponent: ComponentType, props) {
+  async getPageHtml(svelteComponent: SvelteComponentType, props) {
     // TODO если компонент не был проведен,
     // то возвращать бланковую html страницу
 
-    console.log(svelteComponent);
-
     const renderResult = svelteComponent.render(props);
+
+    console.log(renderResult);
 
     this.head = renderResult.head;
     this.body = renderResult.html;
     this.css = renderResult.css.code;
+
+    type Test = HTMLRewriterTypes.HTMLRewriterContentHandlers;
 
     const rewriter = new HTMLRewriter()
       .on("html", new AppendHeadRewriter(this.head))
