2d1ccd679153c4a5a2a08fdc08d20031c2a5d084
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-13 17:41:28 +0300

    refactor(site-ui): change package imports

--- site/site-ui/src/index.ts
+++ site/site-ui/src/index.ts
@@ -1,5 +1,3 @@
-export * from "./components";
 export * from "./fragments";
 export * from "./layouts";
-export * from "./svgs";
 export * from "./pages";
--- site/site-ui/src/pages/index.ts
+++ site/site-ui/src/pages/index.ts
@@ -1,7 +1,1 @@
-export { default as AboutPage } from "./about.page.svelte";
-export { default as ErrorPage } from "./error.page.svelte";
-export { default as HomePage } from "./home.page.svelte";
-export { default as LoginPage } from "./login.page.svelte";
-export { default as NodeExtendedPage } from "./node-extended.page.svelte";
-export { default as NodePage } from "./node.page.svelte";
-export { default as SignupPage } from "./signup.page.svelte";
+export * from "./home.page.tsx";
