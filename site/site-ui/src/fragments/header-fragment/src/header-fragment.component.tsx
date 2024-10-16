90da8211abb67d113f3e76efdd8dcb4095c0c4e9
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-16 07:46:47 +0300

    feat(site-ui/fragments): add header-fragment

--- site/site-ui/src/fragments/header-fragment/src/header-fragment.component.tsx
+++ site/site-ui/src/fragments/header-fragment/src/header-fragment.component.tsx
@@ -1,53 +1,24 @@
-<script>
-  import Logo from "../components/logo.component.svelte";
-  import Hr from "../components/hr.component.svelte";
+import { Hr } from "@ui/hr";
+import { LogoComponent } from "@ui/logo";
 
-  export let bottom = false;
-</script>
+import type { HeaderFragmentType } from "./header-fragment.interface";
 
-<header class="grid" style={bottom ? "margin: auto 0 0;" : ""}>
-  {#if bottom}
-    <Hr text="header" />
-  {/if}
+const HeaderFragment: HeaderFragmentType = (props) => {
+  const { isBottomPosition = false } = props;
 
-  <Logo />
-  <h5 class="header__editor-link">editor</h5>
-  <h5 class="header__login-container">
-    <a href="/login">login</a>
-  </h5>
-</header>
+  return (
+    <>
+      <header class="grid" style={isBottomPosition ? "margin: auto 0 0;" : ""}>
+        {isBottomPosition && <Hr text="header" />}
 
-<style>
-  header * {
-    text-transform: uppercase;
-  }
+        <LogoComponent />
+        <h5 class="header__editor-link">editor</h5>
+        <h5 class="header__login-container">
+          <a href="/login">login</a>
+        </h5>
+      </header>
+    </>
+  );
+};
 
-  :global(.element-layout header) {
-    margin: auto 0 0;
-  }
-
-  .header__login-container {
-    grid-column-end: -1;
-    /* grid-row-start: 1; */
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  @media screen and (max-width: 650px) {
-    .header__editor-link {
-      grid-row: 2 / 3;
-    }
-  }
-
-  @media screen and (max-width: 444px) {
-    .header__editor-link {
-      grid-column: 1 / -1;
-    }
-  }
-
-  @media screen and (max-width: 360px) {
-    .header__login-container {
-      justify-content: flex-start;
-    }
-  }
-</style>
+export { HeaderFragment };
