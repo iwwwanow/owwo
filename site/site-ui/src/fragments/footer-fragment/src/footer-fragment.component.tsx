06009704ed9b8db1d54fd1afe34d847f2727a3f5
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-16 07:46:40 +0300

    feat(site-ui/fragments): add footer-fragment

--- site/site-ui/src/fragments/footer-fragment/src/footer-fragment.component.tsx
+++ site/site-ui/src/fragments/footer-fragment/src/footer-fragment.component.tsx
@@ -1,21 +1,17 @@
-<script>
-  import Hr from "../components/hr.component.svelte";
-</script>
+import { Hr } from "@ui/hr";
 
-<footer class="grid footer">
-  <Hr text="footer" />
+const FooterFragment = () => {
+  return (
+    <>
+      <footer class="grid footer">
+        <Hr text="footer" />
 
-  <h5>
-    <a href="/about">о проекте</a>
-  </h5>
-</footer>
+        <h5>
+          <a href="/about">о проекте</a>
+        </h5>
+      </footer>
+    </>
+  );
+};
 
-<style>
-  .footer {
-    margin: auto 0 0;
-  }
-
-  .footer * {
-    text-transform: uppercase;
-  }
-</style>
+export { FooterFragment };
--- site/site-ui/src/fragments/footer-fragment/src/footer.fragment.svelte
+++ site/site-ui/src/fragments/footer-fragment/src/footer.fragment.svelte
@@ -2,20 +2,5 @@
   import Hr from "../components/hr.component.svelte";
 </script>
 
-<footer class="grid footer">
-  <Hr text="footer" />
-
-  <h5>
-    <a href="/about">о проекте</a>
-  </h5>
-</footer>
-
 <style>
-  .footer {
-    margin: auto 0 0;
-  }
-
-  .footer * {
-    text-transform: uppercase;
-  }
 </style>
