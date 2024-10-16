d30c65196b12e7364fba6802bc67517e6ac26e2e
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-16 07:47:38 +0300

    refactor(site-ui/layouts): rm unused

--- site/site-ui/src/layouts/node-extended.layout.svelte
+++ site/site-ui/src/layouts/node-extended.layout.svelte
@@ -1,19 +1,4 @@
-<script>
-  import BaseHead from "../fragments/base-head.fragment.svelte";
-</script>
-
-<svelte:head>
-  <BaseHead />
-</svelte:head>
-
-<body class="node-extended-layout">
-  <slot />
-</body>
+<svelte:head></svelte:head>
 
 <style>
-  .node-extended-layout {
-    display: flex;
-    flex-direction: column;
-    gap: 80px;
-  }
 </style>
