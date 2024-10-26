361d23e85ce7189136eea01d89c21ca2a38d06f3
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-26 13:15:35 +0300

    feat(interfaces): exports

--- site/interfaces/src/index.ts
+++ site/interfaces/src/index.ts
@@ -1,4 +1,5 @@
-export * from "./node.interface";
 export * from "./node-image.interface";
 export * from "./node-content.interface";
 export * from "./node-date.interface";
+export * from "./node-data.interface";
+export * from "./client.interface";
