d8feaa4a65037a966ab06e5b451566235352c537
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-30 11:06:00 +0300

    refactor(elysia): import signupdata-interface

--- site/elysia-app/src/getters/signup-data.interface.ts
+++ site/elysia-app/src/getters/signup-data.interface.ts
@@ -1,13 +1,10 @@
+import type { SignupData } from "@site/interfaces";
 import type { Static } from "elysia";
 
 import { SignupPostType } from "../interfaces";
 
 type GetSignupDataBodyProp = Static<(typeof SignupPostType)["body"]>;
 
-type GetSignupDataType = (body: GetSignupDataBodyProp) => {
-  username: string;
-  password: string;
-  confirmPassword: string;
-};
+type GetSignupDataType = (body: GetSignupDataBodyProp) => SignupData;
 
 export type { GetSignupDataType };
