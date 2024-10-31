d910b630f3af28211fad9dc4faf1900827579b75
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-10-31 07:48:46 +0300

    feat(controllers): signup-controller resultWriteData

--- site/controllers/src/signup-controller/signup.controller.ts
+++ site/controllers/src/signup-controller/signup.controller.ts
@@ -4,8 +4,8 @@ import { AccountModel } from "@site/models";
 class SignupController {
   static async processSignup(signupData: SignupData) {
     const account = new AccountModel(signupData);
-    await account.writeData();
-    return console.log(signupData);
+    const writeDataResult = await account.writeData();
+    console.log("writeDataResult: ", writeDataResult);
   }
 }
 
