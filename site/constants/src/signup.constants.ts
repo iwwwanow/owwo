158060708e0538c281162494d3c0047302014765
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-04 15:17:40 +0300

    feat: todos

--- site/constants/src/signup.constants.ts
+++ site/constants/src/signup.constants.ts
@@ -1,3 +1,5 @@
+// TODO use that names from schema
 export const USERNAME_INPUT_NAME = "username";
 export const PASSWORD_INPUT_NAME = "password";
+
 export const CONFIRM_PASSWORD_INPUT_NAME = "confirm-password";
