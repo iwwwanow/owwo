220d2b4a391428d62903a59189d93a7a18249897
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-04 15:19:20 +0300

    refactor(elysia-app): rm unused

--- site/elysia-app/src/getters/env-error-message.getter.ts
+++ site/elysia-app/src/getters/env-error-message.getter.ts
@@ -1,3 +1,5 @@
+// TODO you can use typebox on check envs
+// TODO can you use typebox on check envs?
 const getEnvErrorMessage = (envName: string) =>
   `need "${envName}" env variable`;
 
--- site/elysia-app/src/getters/index.ts
+++ site/elysia-app/src/getters/index.ts
@@ -1,3 +1,2 @@
 export * from "./env-error-message.getter";
 export * from "./launch-text.getter";
-export * from "./signup-data.getter";
