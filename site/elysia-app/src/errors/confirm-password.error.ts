81077c2aeaa9a85852c02382ad5dc96f60d6a5aa
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-07 10:04:46 +0300

    feat(elysia-app): add confirm-password error

---
+++ site/elysia-app/src/errors/confirm-password.error.ts
@@ -0,0 +1,9 @@
+// TODO create parent class for all client errors
+export class ConfirmPasswordError extends Error {
+  // TODO locales
+  clientMessage = "confirm-password missmatch";
+
+  constructor() {
+    super("error on site_elysia-app_signup-service");
+  }
+}
