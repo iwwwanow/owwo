91910894daaff9a2b5613be00dcbd5c00330636c
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-11 07:45:27 +0300

    refactor(elysia/middlewares): mv all signup logic to middlewares

---
+++ site/elysia-app/src/middlewares/signup.middleware.ts
@@ -0,0 +1,48 @@
+import { CONFIRM_PASSWORD_INPUT_NAME } from "@site/constants";
+import { SignupController } from "@site/controllers";
+import type { SignupDtoType } from "@site/dto";
+import type { redirect as RedirectType } from "elysia";
+
+import { UniqueUsernameError } from "../errors";
+import { ConfirmPasswordError } from "../errors";
+import { confirmPasswordValidator } from "../validators";
+import { UNIQUE_USERNAME_ORM_ERROR_MESSAGE } from "./signup.constants";
+
+// TODO move to interfaces
+type PropsType = {
+  signupData: SignupDtoType;
+  redirect: RedirectType;
+};
+
+export class SignupMiddleware {
+  static async processPostRequest(props: PropsType) {
+    const { signupData, redirect } = props;
+
+    const {
+      password,
+      username,
+      [CONFIRM_PASSWORD_INPUT_NAME]: confirmPassword,
+    } = signupData;
+
+    const isPasswordValid = confirmPasswordValidator(password, confirmPassword);
+    if (!isPasswordValid) throw new ConfirmPasswordError();
+
+    try {
+      await SignupController.processSignup({ username, password });
+
+      // TODO success message
+      return redirect("/");
+    } catch (error) {
+      // TODO check error type and rende it on client
+
+      // TODO to const
+      if (error.message === UNIQUE_USERNAME_ORM_ERROR_MESSAGE) {
+        throw new UniqueUsernameError();
+      }
+
+      console.error("ERROR ON SIGNUP SERVICE:");
+      console.error(error);
+      throw new Error("signup-controller undefined error");
+    }
+  }
+}
