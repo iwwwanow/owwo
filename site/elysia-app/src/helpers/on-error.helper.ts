12ccea8783375b926ff10279b8be0e8ee8ceb0bd
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-11 07:44:38 +0300

    refactor(elysia/helpers): rm unused from on-error-helper

--- site/elysia-app/src/helpers/on-error.helper.ts
+++ site/elysia-app/src/helpers/on-error.helper.ts
@@ -1,41 +1,8 @@
 // TODO он делается не аналогии с другими сервисами. это не экземпляр класса, объект а функция - helper
+
+// TODO provide dto
 export const onErrorHelper = (ctx) => {
-  const { error, code, redirect } = ctx;
-
-  // console.log("error");
-  // return redirect("/");
-
-  console.log("error");
-  return "error";
-  return new Response("error");
-
-  return "blaaa";
-
-  console.log("ERROR ON ON ERROR HANDLER:");
-
-  // TODO перенаправлять пользователя на ту же страницу, где была совершена обишка, только с объектом уведомнелия
-
-  // TODO render error-code on client
-
-  // TODO можно для каждой проверки валидации написать свой текст ошибки
-  // и выводить её на клиент
-  // https://elysiajs.com/essential/validation.html#onerror
-
-  // TODO сделать локализацию в виде json для передачи ошибок на клиент
-  // можно в текущих макетах добавить поле error, на самом верху странице, под шапкой. чтобы при ошибке отображать ту странциу, с которой она ушла, только с ТЕКСТОМ ЭТОЙ ОШИБКИ
-
-  if (code === "VALIDATION") {
-    console.error(error);
-    console.error("error-validation-handing");
-    return `<>${error}</>`;
-  }
-
-  console.log(error.clientMessage);
-
-  return redirect("/");
-
-  // return `<>${error.clientMessage}</>`;
-  // return new Response(error.toString());
-
-  return new Response("hi");
+  const { error, code } = ctx;
+  console.log("error on helper");
+  return "bla";
 };
