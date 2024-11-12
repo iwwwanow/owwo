2ff8b7083fcd5af0bc4b83bc29f018a60b332b41
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-12 21:36:30 +0300

    feat(elysia): on-error-helper render error data on client

--- site/elysia-app/src/helpers/on-error.helper.ts
+++ site/elysia-app/src/helpers/on-error.helper.ts
@@ -1,8 +1,13 @@
+import { ViewController } from "@site/controllers";
+
 // TODO он делается не аналогии с другими сервисами. это не экземпляр класса, объект а функция - helper
 
 // TODO provide dto
 export const onErrorHelper = (ctx) => {
   const { error, code } = ctx;
-  console.log("error on helper");
-  return "bla";
+  console.log(error);
+  return ViewController.getErrorPage({
+    errorMessage: error.clientMessage,
+    errorCode: code,
+  });
 };
