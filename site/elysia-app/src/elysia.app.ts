2cd53ce38c4b3bbf6ab55357002bf3a016fcf809
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-15 07:58:38 +0300

    refactor(elysia): move on error directrly to elysia

--- site/elysia-app/src/elysia.app.ts
+++ site/elysia-app/src/elysia.app.ts
@@ -1,14 +1,28 @@
+import { ViewController } from "@site/controllers";
 import { Elysia } from "elysia";
 
 import { LISTEN_PORT } from "./elysia.constants.js";
-import { onErrorHelper } from "./helpers/index.js";
+import { ClientError } from "./errors/_client.error.js";
 import { routerService } from "./services/index.js";
 import { signupService } from "./services/index.js";
 import { faviconService } from "./services/index.js";
 import { staticService } from "./services/index.js";
 
 const app = new Elysia()
-  .onError(onErrorHelper)
+  .onError((ctx) => {
+    const { error: e, code } = ctx;
+    const error = e as unknown as ClientError | Error;
+
+    if (error instanceof ClientError) {
+      return ViewController.getErrorPage({
+        errorMessage: error.clientMessage,
+        errorCode: code,
+      });
+    }
+
+    console.error(error);
+    return error.message;
+  })
   .use(faviconService)
   .use(staticService)
   .use(routerService)
