21ae1be161d29ebf2f098950b975f0bc045b25a0
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-19 07:28:11 +0300

    refactor(site/controlelrs): rm unused from view-controller

--- site/controllers/src/view-controller/view.controller.ts
+++ site/controllers/src/view-controller/view.controller.ts
@@ -14,27 +14,14 @@ import type { GetHomePageProps } from "./view.interfaces";
 import type { GetErrorPageProps } from "./view.interfaces";
 
 class ViewController {
-  // static async responsePageHtml(svelteComponent, props) {
-  //   // TODO svetle page type
-  //
-  //   const pageHtml = await bte.getPageHtml(svelteComponent, props);
-  //   return html(pageHtml);
-  // }
-
   static async getHomePage({ successMessage }: GetHomePageProps) {
-    // TODO add conditions to test data render
     const TEST_USERS_QUANTITY = 128;
-    // const nodeId = process.env["TEST_NODE_USERNAME"];
     const nodeData = await MockModel.getUserNodeData();
     const users = Array(TEST_USERS_QUANTITY).fill(nodeData);
 
     const clientData = { successMessage };
 
     const props = { clientData, users };
-
-    // TODO
-    // 1. на продакшене должен быть оптимизированый СБИЛЖЕННЫЙ css.
-    // 2. в дев режиме нужно собирать все стили в кучу и отправлять под каждым компонентом
 
     return HomePage(props);
   }
@@ -62,28 +49,20 @@ class ViewController {
     return SignupPage(props);
   }
 
-  // static async renderNodePage(nodeId: string, options: RenderNodePageOptions) {
   static async getNodePage(nodeId: string) {
-    // TODO dev mode is editor true
-    const client = new ClientModel({ isEditor: true });
+    const isDevelopement = process.env.NODE_ENV === "developement";
+
+    const client = new ClientModel({
+      isEditor: isDevelopement ? true : false,
+    });
 
     const node = new NodeTestModel(nodeId);
     const nodeData = await node.getData();
 
-    // const EDITOR_URL = new URL("", req.url);
-    // EDITOR_URL.searchParams.set("editor", true);
-    // console.log(`EDITOR URL: ${EDITOR_URL.href}`);
-    //
-    //
-    // const REQUEST_URL = new URL("", req.url);
-    // if (REQUEST_URL.searchParams.has("editor", "true")) {
-    //   client.isEditor = true;
-    //
     //   // client.type = 'creator'
     //   // client.type = 'author'
     //   // client.type = 'pusher'
     //   // client.type = 'viewer'
-    // }
 
     const props = { client, nodeData };
 
