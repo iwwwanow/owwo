b7fdf8aa4b5f38941c0c4235f51cd4a546d8ec2d
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-21 15:40:21 +0300

    refactor(ui/node-card): change image variants to consts

--- ui/node-card/src/node-card__image/node-card__image.component.tsx
+++ ui/node-card/src/node-card__image/node-card__image.component.tsx
@@ -1,16 +1,19 @@
-// TODO;
-// move image-varian-names to global constants
-// and use it all applicatoin wide
+import { IMAGE_VARIANT_NAME } from "@site/constants";
+
 import type { NodeCardImageType } from "./node-card__image.interfaces";
 
 export const NodeCardImage: NodeCardImageType = ({ image }) => {
-  const image2xSrcset = `${image.w190}, ${image.w190_2x} 2x`;
+  const imageWidth190pxSource = image[IMAGE_VARIANT_NAME.width190px];
+  const imageWidth190px2xSource = image[IMAGE_VARIANT_NAME.width190px2x];
+  const imageOriginalSource = image[IMAGE_VARIANT_NAME.original];
+
+  const image2xSrcset = `${imageWidth190pxSource}, ${imageWidth190px2xSource} 2x`;
 
   return (
     <picture class="card__cover-container">
-      <source srcset={image.w190_2x} media="(max-width: 360px)" />
+      <source srcset={imageWidth190px2xSource} media="(max-width: 360px)" />
       <source srcset={image2xSrcset} />
-      <img src={image.original} class="card__img" alt="page card cover" />
+      <img src={imageOriginalSource} class="card__img" alt="page card cover" />
     </picture>
   );
 };
