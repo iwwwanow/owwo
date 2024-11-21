b10d67825316cc7b46d1638b0eaf30bf248e8a2a
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-21 15:39:20 +0300

    feat(site/constants): add image-variants

---
+++ site/constants/src/image-variant.constants.ts
@@ -0,0 +1,11 @@
+export enum IMAGE_VARIANT_NAME {
+  blob = "blob",
+  original = "original",
+  height16px = "h16",
+  height16px2x = "h16_2x",
+  height32px = "h32",
+  height32px2x = "h32_2x",
+  width1080px = "w1080",
+  width190px = "w190",
+  width190px2x = "w190_2x",
+}
--- site/constants/src/index.ts
+++ site/constants/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./mock.constants";
 export * from "./signup.constants";
 export * from "./router.constants";
+export * from "./image-variant.constants";
