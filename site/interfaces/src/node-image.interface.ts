67db523b2142e2d2431964e99ff33684f86b8d49
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-21 15:39:47 +0300

    refactor(site/interfaces): change image type keys to const variants

--- site/interfaces/src/node-image.interface.ts
+++ site/interfaces/src/node-image.interface.ts
@@ -1,13 +1,15 @@
+import { IMAGE_VARIANT_NAME } from "@site/constants";
+
 type NodeImageType = {
-  blob: string;
-  original: string;
-  h16: string;
-  h16_2x: string;
-  h32: string;
-  h32_2x: string;
-  w1080: string;
-  w190: string;
-  w190_2x: string;
+  [IMAGE_VARIANT_NAME.blob]: string;
+  [IMAGE_VARIANT_NAME.original]: string;
+  [IMAGE_VARIANT_NAME.height16px]: string;
+  [IMAGE_VARIANT_NAME.height16px2x]: string;
+  [IMAGE_VARIANT_NAME.height32px]: string;
+  [IMAGE_VARIANT_NAME.height32px2x]: string;
+  [IMAGE_VARIANT_NAME.width1080px]: string;
+  [IMAGE_VARIANT_NAME.width190px]: string;
+  [IMAGE_VARIANT_NAME.width190px2x]: string;
 };
 
 export type { NodeImageType };
