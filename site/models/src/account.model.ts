c7590b8c06d10d9dc64da9685686dc622bc56814
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-26 22:23:50 +0300

    feat(site/models): add find and delete methods

--- site/models/src/account.model.ts
+++ site/models/src/account.model.ts
@@ -1,17 +1,30 @@
 import { mainDb } from "@db/main";
 import { accountsTable } from "@db/main";
-import type { SignupDataType } from "@site/dto";
+import type { AccountDataType } from "@site/dto";
+import { eq } from "drizzle-orm";
 
 class AccountModel {
-  signupData: SignupDataType;
+  accountData: AccountDataType;
 
-  constructor(signupData: SignupDataType) {
-    this.signupData = signupData;
+  constructor(accountData: AccountDataType) {
+    this.accountData = accountData;
   }
 
   async writeData() {
-    const { username, password } = this.signupData;
-    await mainDb.insert(accountsTable).values({ username, password });
+    await mainDb.insert(accountsTable).values(this.accountData);
+  }
+
+  async getData() {
+    return mainDb
+      .select()
+      .from(accountsTable)
+      .where(eq(accountsTable.username, this.accountData.username));
+  }
+
+  async deleteData() {
+    return mainDb
+      .delete(accountsTable)
+      .where(eq(accountsTable.username, this.accountData.username));
   }
 }
 
