e446404c63c353d614f623370531d960577f70f7
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-11-30 14:11:57 +0300

    feat(test): mocks

--- test/mock/src/mocks/index.ts
+++ test/mock/src/mocks/index.ts
@@ -3,3 +3,6 @@ export * from "./classname.mock";
 export * from "./title.mock";
 export * from "./node-data.mock";
 export * from "./node-meta.mock";
+export * from "./symbol.mock";
+export * from "./node-image.mock";
+export * from "./children.mock";
