cdf130e353bdc389b5c99ca1c03ddfbf58de672a
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2024-12-01 13:06:06 +0300

    feat(ui): tests

--- ui/text/src/text.test.ts
+++ ui/text/src/text.test.ts
@@ -1,7 +1,7 @@
 import { nodeContentMock } from "@test/mock";
 import { classnameMock } from "@test/mock";
+import { JsxMarkupTest } from "@test/mock";
 import { describe } from "bun:test";
-import { expect } from "bun:test";
 import { test } from "bun:test";
 
 import { Text } from "./text.component";
@@ -10,11 +10,14 @@ describe("ui-text-test", () => {
   test("markup-test", async () => {
     const requiredComponentString = `<div class="text ${classnameMock}">${nodeContentMock.html}</div>`;
 
-    const textResult = await Text({
-      text: nodeContentMock,
-      className: classnameMock,
+    const markupTest = new JsxMarkupTest({
+      requiredComponentString,
+      renderedComponentString: await Text({
+        text: nodeContentMock,
+        className: classnameMock,
+      }),
     });
 
-    expect(textResult).toBe(requiredComponentString);
+    markupTest.process();
   });
 });
