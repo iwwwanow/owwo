2937f7799940e34c99ad5c69d901d7b015fd1dbe
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-01 14:55:16 +0300

    fix(adapters): change main find method

--- adapters/bun-http-server-adapter/src/helpers/find-route.helper.ts
+++ adapters/bun-http-server-adapter/src/helpers/find-route.helper.ts
@@ -2,7 +2,7 @@ import type { RouteType } from "@contexts/site-core";
 
 export const findRouteHelper = (routes: Array<RouteType>, pathname: string) => {
   const findedRoute = routes.find((route) => {
-    return pathname.includes(route.path);
+    return pathname.startsWith(route.path);
   });
   return findedRoute;
 };
