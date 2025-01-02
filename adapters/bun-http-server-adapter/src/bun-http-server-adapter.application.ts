f202d78578acbbbfdf7774e8f7764c85d3a043fa
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-02 19:40:18 +0300

    feat(adapters): handle errors logic

--- adapters/bun-http-server-adapter/src/bun-http-server-adapter.application.ts
+++ adapters/bun-http-server-adapter/src/bun-http-server-adapter.application.ts
@@ -1,20 +1,25 @@
 import type { HttpServerPort } from "@contexts/site-core";
 import type { InitProps } from "@contexts/site-core";
 import type { RouteType } from "@contexts/site-core";
+import type { SiteError } from "@contexts/site-core";
+import type { HttpServerErrorHandlers } from "@contexts/site-core";
 
 import { sortRoutesHelper } from "./helpers";
 import { findRouteHelper } from "./helpers";
 
 export class BunHttpServerAdapter implements HttpServerPort {
   routes: Array<RouteType> = [];
+  errorHandlers: HttpServerErrorHandlers;
 
-  async init({ port }: InitProps) {
+  async init({ port, errorHandlers }: InitProps) {
+    this.errorHandlers = errorHandlers;
     this.listen(port);
     console.log("init bun http server adapter");
   }
 
   listen(port: number) {
     const routes = this.routes;
+    const errorHandlers = this.errorHandlers;
 
     const { url } = Bun.serve({
       port: port,
@@ -31,11 +36,15 @@ export class BunHttpServerAdapter implements HttpServerPort {
           if (findedRoute) {
             return findedRoute.handler(req);
           }
-        } catch (error) {
-          if ((error.code = 404)) return new Response("page not found");
+        } catch (e: unknown) {
+          const error = e as SiteError;
+
+          if (error.code === 404) {
+            return errorHandlers.pageNotFoundErrorHandler(req);
+          }
         }
 
-        return new Response("internal server error");
+        return errorHandlers.internalServerErrorHandler(req);
       },
     });
 
