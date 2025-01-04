69fe5ba8f536221d58acfe94eeb50f047224bc47
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-04 22:41:49 +0300

    feat(adapters): import index page WIP

--- adapters/svelte-adapter/src/svelte-adapter.application.ts
+++ adapters/svelte-adapter/src/svelte-adapter.application.ts
@@ -1,11 +1,14 @@
 import type { SiteViewPort } from "@contexts/site-core";
 
+import { IndexPage } from "./pages";
+
 export class SvelteAdapter implements SiteViewPort {
   async init(): Promise<void> {
     console.log("init svelte adapter");
   }
 
   async getHomePage(): Promise<string> {
+    console.log(IndexPage.render());
     return "<h1>svelte-home-page</h1>";
   }
 }
