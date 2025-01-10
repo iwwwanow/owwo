510b34e3486e72dca3013a7fb2ff9a7e8d7db882
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-10 13:40:48 +0300

    refactor(tests): namings

--- tests/site-tests/src/mocks/node-image.mock.ts
+++ tests/site-tests/src/mocks/node-image.mock.ts
@@ -2,13 +2,13 @@ import { ImageVariantName } from "@contexts/site-core";
 import type { NodeImageType } from "@contexts/site-core";
 
 export const nodeImageMock: NodeImageType = {
-  [ImageVariantName.BLOB]: "node-image-blob-mock-string",
-  [ImageVariantName.ORIGINAL]: "node-image-blob-original-mock-string",
-  [ImageVariantName.HEIGHT_16PX]: "node-image-height-16px-mock-string",
-  [ImageVariantName.HEIGHT_16PX_2X]: "node-image-height-16px-2x-mock-string",
-  [ImageVariantName.HEIGHT_32PX]: "node-image-height-32px-mock-string",
-  [ImageVariantName.HEIGHT_32PX_2X]: "node-image-height-32px-2x-mock-string",
-  [ImageVariantName.WIDTH_1080PX]: "node-image-width-1080px-mock-string",
-  [ImageVariantName.WIDTH_190PX]: "node-image-width-190px-mock-string",
-  [ImageVariantName.WIDTH_190PX_2X]: "node-image-width-190px-2x-mock-string",
+  [ImageVariantName.Blob]: "node-image-blob-mock-string",
+  [ImageVariantName.Original]: "node-image-blob-original-mock-string",
+  [ImageVariantName.Height_16px]: "node-image-height-16px-mock-string",
+  [ImageVariantName.Height_16px_2x]: "node-image-height-16px-2x-mock-string",
+  [ImageVariantName.Height_32px]: "node-image-height-32px-mock-string",
+  [ImageVariantName.Height_32px_2x]: "node-image-height-32px-2x-mock-string",
+  [ImageVariantName.Width_1080px]: "node-image-width-1080px-mock-string",
+  [ImageVariantName.Width_190px]: "node-image-width-190px-mock-string",
+  [ImageVariantName.Width_190px_2x]: "node-image-width-190px-2x-mock-string",
 };
