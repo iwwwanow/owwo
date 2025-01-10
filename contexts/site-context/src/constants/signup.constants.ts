6ab2fc6004c4a6e925241b1ee1df709e1f18af11
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-10 13:39:39 +0300

    refactor(contexts): imports

--- contexts/site-context/src/constants/signup.constants.ts
+++ contexts/site-context/src/constants/signup.constants.ts
@@ -1,5 +1,7 @@
-import { USERNAME_COLUMN_NAME } from "@db/main";
-import { PASSWORD_COLUMN_NAME } from "@db/main";
+// import { USERNAME_COLUMN_NAME } from "@db/main";
+// import { PASSWORD_COLUMN_NAME } from "@db/main";
+const USERNAME_COLUMN_NAME = 'username'
+const PASSWORD_COLUMN_NAME = 'password'
 
 export const USERNAME_INPUT_NAME = USERNAME_COLUMN_NAME;
 export const PASSWORD_INPUT_NAME = PASSWORD_COLUMN_NAME;
