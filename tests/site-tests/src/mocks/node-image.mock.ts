9056a561dc2e79500a5187e40c020743ee558f83
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-12 14:29:25 +0300

    refactor(tests): image-mocks

--- tests/site-tests/src/mocks/node-image.mock.ts
+++ tests/site-tests/src/mocks/node-image.mock.ts
@@ -2,13 +2,33 @@ import { ImageVariantName } from "@contexts/site-core";
 import type { NodeImageType } from "@contexts/site-core";
 
 export const nodeImageMock: NodeImageType = {
-  [ImageVariantName.Blob]: "node-image-blob-mock-string",
-  [ImageVariantName.Original]: "node-image-blob-original-mock-string",
-  [ImageVariantName.Height_16px]: "node-image-height-16px-mock-string",
-  [ImageVariantName.Height_16px_2x]: "node-image-height-16px-2x-mock-string",
-  [ImageVariantName.Height_32px]: "node-image-height-32px-mock-string",
-  [ImageVariantName.Height_32px_2x]: "node-image-height-32px-2x-mock-string",
-  [ImageVariantName.Width_1080px]: "node-image-width-1080px-mock-string",
-  [ImageVariantName.Width_190px]: "node-image-width-190px-mock-string",
-  [ImageVariantName.Width_190px_2x]: "node-image-width-190px-2x-mock-string",
+  [ImageVariantName.Blob]: "https://images.placeholders.dev/?width=8&height=8",
+  [ImageVariantName.Original]:
+    "https://images.placeholders.dev/?width=1080&height=1080",
+  [ImageVariantName.Height_16px]:
+    "https://images.placeholders.dev/?width=16&height=16",
+  [ImageVariantName.Height_16px_2x]:
+    "https://images.placeholders.dev/?width=32&height=32",
+  [ImageVariantName.Height_32px]:
+    "https://images.placeholders.dev/?width=32&height=32",
+  [ImageVariantName.Height_32px_2x]:
+    "https://images.placeholders.dev/?width=64&height=64",
+  [ImageVariantName.Width_1080px]:
+    "https://images.placeholders.dev/?width=1080&height=1080",
+  [ImageVariantName.Width_190px]:
+    "https://images.placeholders.dev/?width=190&height=190",
+  [ImageVariantName.Width_190px_2x]:
+    "https://images.placeholders.dev/?width=380&height=380",
 };
+
+// export const IMAGE_TEST_DATA_COVER = {
+//   blob: "https://images.placeholders.dev/?width=8&height=8",
+//   original: "https://images.placeholders.dev/?width=1080&height=1080",
+//   h16: "https://images.placeholders.dev/?width=10&height=16",
+//   h16_2x: "https://images.placeholders.dev/?width=22&height=32",
+//   h32: "https://images.placeholders.dev/?width=22&height=32",
+//   h32_2x: "https://images.placeholders.dev/?width=42&height=64",
+//   w190: "https://images.placeholders.dev/?width=190&height=288",
+//   w190_2x: "https://images.placeholders.dev/?width=380&height=576",
+//   w1080: "https://images.placeholders.dev/?width=1080&height=1080",
+// };
