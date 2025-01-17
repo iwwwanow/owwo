c6d1980b70afd2acc3cfe43baa5638a43d240ecb
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-17 20:22:00 +0300

    feat(context): site-view ports

--- contexts/site-context/src/ports/site-view.port.ts
+++ contexts/site-context/src/ports/site-view.port.ts
@@ -2,4 +2,17 @@ export interface SiteViewPort {
   init(): Promise<void>;
 
   getIndexPage(): Promise<string>;
+
+  getLoginPage(): Promise<string>;
+
+  getSignupPage(): Promise<string>;
+
+  getAboutPage(): Promise<string>;
+
+  // is it used?
+  getErrorPage(): Promise<string>;
+
+  getNodeExtendedPage(): Promise<string>;
+
+  getNodePage(): Promise<string>;
 }
