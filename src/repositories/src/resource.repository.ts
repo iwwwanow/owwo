71849ba8a8e8349c0816938443caacc558c3c855
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-24 20:39:47 +0300

    feat(repository): page type resolver

--- src/repositories/src/resource.repository.ts
+++ src/repositories/src/resource.repository.ts
@@ -1,14 +1,40 @@
 import type { Resource } from "@site/models";
-import { dir } from "console";
+import { PageType } from "@site/models";
+import { create } from "domain";
 import { readdir } from "fs/promises";
 import { join } from "path";
 
 export class ResourceRepository {
-  static async getResourceData(path: string): Resource {
+  private static resolvePageType(path: string): PageType {
+    // const segments = path.split("/");
+
+    if (path === "/") return PageType.Index;
+    else if (path === "/about") return PageType.About;
+
+    return PageType.Common;
+  }
+
+  static async getByPath(path: string): Promise<{
+    pageType: PageType;
+    resourceData: Resource;
+  }> {
+    const pageType = ResourceRepository.resolvePageType(path);
+
     const dirData = await readdir(path);
     console.log(dirData);
     for (const data of dirData) {
       console.log(Bun.file(join(path, data)));
     }
+
+    const resourceData = {
+      meta: {
+        title: "title",
+        path,
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+      },
+    };
+
+    return { pageType, resourceData };
   }
 }
