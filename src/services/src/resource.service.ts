b8d088c7268d2676efdc8d76db7867a93ea81ee3
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-24 20:40:07 +0300

    feat(services): presentation

--- src/services/src/resource.service.ts
+++ src/services/src/resource.service.ts
@@ -1,16 +1,24 @@
+import { ResourcePage } from "@site/presentation";
 import { ResourceRepository } from "@site/repositories";
 
 import { getUploadsPath } from "./getters";
 import { getResourcePath } from "./getters";
 
 export class ResourceService {
-  static async get(req: Request) {
+  // TODO уровень бизнес логики
+  // TODO упаковать работу с presentation на этот уровень. контроллер только отпралвяет html
+  // getResourcePresentation
+  static async getPresentation(req: Request): Promise<string> {
     const reqUrl = new URL(req.url);
     const { pathname: reqPathname } = reqUrl;
 
     const uploadsPath = getUploadsPath();
     const resourcePath = getResourcePath(uploadsPath, reqPathname);
 
-    const resourceData = await ResourceRepository.getResourceData(resourcePath);
+    const resourceData = await ResourceRepository.getByPath(resourcePath);
+
+    const resourcePageHtml = ResourcePage({ resourceData });
+
+    return resourcePageHtml;
   }
 }
