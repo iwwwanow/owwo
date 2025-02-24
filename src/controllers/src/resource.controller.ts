7c935522089bb4cda18a0b9671156c9772f3dcf3
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-24 20:38:50 +0300

    refactor(controllers): mv presentation login into service

--- src/controllers/src/resource.controller.ts
+++ src/controllers/src/resource.controller.ts
@@ -1,14 +1,11 @@
-import { ResourcePage } from "@site/presentation";
 import { ResourceService } from "@site/services";
 
 import { getResponseHtml } from "./getters";
 
 export class ResourceController {
   static async get(req: Request) {
-    // TODO
-    const resourceData = ResourceService.get(req);
-    const html = ResourcePage({ resourceData });
-    const response = getResponseHtml(html);
+    const resourcePageHtml = await ResourceService.getPresentation(req);
+    const response = getResponseHtml(resourcePageHtml);
     return response;
   }
 }
