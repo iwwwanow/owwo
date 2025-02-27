8cff6aac89c99f41f9aa953c143efe63233ac60a
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-02-27 20:56:02 +0300

    refactor(repository): rm unused

--- src/repositories/src/resource.repository.ts
+++ src/repositories/src/resource.repository.ts
@@ -1,40 +1,11 @@
-import type { Resource } from "@site/models";
-import { PageType } from "@site/models";
-import { create } from "domain";
+import { ResourceModel } from "@site/models";
 import { readdir } from "fs/promises";
 import { join } from "path";
 
 export class ResourceRepository {
-  private static resolvePageType(path: string): PageType {
-    // const segments = path.split("/");
-
-    if (path === "/") return PageType.Index;
-    else if (path === "/about") return PageType.About;
-
-    return PageType.Common;
-  }
-
-  static async getByPath(path: string): Promise<{
-    pageType: PageType;
-    resourceData: Resource;
-  }> {
-    const pageType = ResourceRepository.resolvePageType(path);
-
-    const dirData = await readdir(path);
-    console.log(dirData);
-    for (const data of dirData) {
-      console.log(Bun.file(join(path, data)));
-    }
-
-    const resourceData = {
-      meta: {
-        title: "title",
-        path,
-        createdAt: new Date(Date.now()),
-        updatedAt: new Date(Date.now()),
-      },
-    };
-
-    return { pageType, resourceData };
+  static async getByPath(path: string): Promise<ResourceModel> {
+    const resource = new ResourceModel(path);
+    await resource.init();
+    return resource;
   }
 }
