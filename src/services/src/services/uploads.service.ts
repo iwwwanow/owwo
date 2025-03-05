5b5c3d72f337920cf462d50bb1f0001e4a0b1451
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-03-05 18:57:39 +0300

    fix(services): decode uri

--- src/services/src/services/uploads.service.ts
+++ src/services/src/services/uploads.service.ts
@@ -3,13 +3,12 @@ import { join } from "path";
 
 import { getUploadsPath } from "../getters/index.js";
 
-const PUBLIC_PATH = "./src/static";
-
 export class UploadsService {
   static async getFile(req: Request): Promise<BunFile> {
     const reqUrl = new URL(req.url);
     const { pathname: reqPathname } = reqUrl;
-    const slug = reqPathname.split("/").slice(2).join("/");
+    const decodedPathname = decodeURI(reqPathname);
+    const slug = decodedPathname.split("/").slice(2).join("/");
     const uploadsPath = getUploadsPath();
     const filePath = join(uploadsPath, slug);
     return Bun.file(filePath);
