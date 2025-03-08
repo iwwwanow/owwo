c83e3760a3154f8384ffb3a5e7f32c99cbdbefee
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-03-08 18:42:01 +0300

    feat(common): favicon

--- src/routes/src/routes/routes.ts
+++ src/routes/src/routes/routes.ts
@@ -2,13 +2,11 @@ import { ResourceController } from "@site/controllers";
 import { PublicController } from "@site/controllers";
 import { UploadsController } from "@site/controllers";
 
-import type { App } from "../interfaces";
+import type { App } from "../interfaces/index.js";
 
 export const registerRoutes = (app: App) => {
   app.get("/favicon.ico", (req: Request) => {
-    console.log("TODO FAVICON!");
-    return new Response("static route");
-    // ResourceController.get(req)
+    return PublicController.get(req);
   });
 
   app.get("/public", (req: Request) => {
--- src/services/src/services/public.service.ts
+++ src/services/src/services/public.service.ts
@@ -8,7 +8,18 @@ export class PublicService {
   static async getFile(req: Request): Promise<BunFile> {
     const reqUrl = new URL(req.url);
     const { pathname: reqPathname } = reqUrl;
-    const filePath = join(PUBLIC_PATH, reqPathname);
+
+    let filePath: string;
+
+    if (reqPathname === "/favicon.ico") {
+      console.log("favicon");
+      // TODO favicon name to const
+      // TODO all path to const; and check in on start with warning
+      filePath = join(PUBLIC_PATH, "public", "assets", "favicon.png");
+    } else {
+      filePath = join(PUBLIC_PATH, reqPathname);
+    }
+
     return Bun.file(filePath);
   }
 }
