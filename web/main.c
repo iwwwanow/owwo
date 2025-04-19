4f7777b2d55934e1159d67a0e45312735ada38ae
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-19 06:02:04 +0300

    feat(web): websockets script

--- web/main.c
+++ web/main.c
@@ -1,12 +1,35 @@
 #include <fcgi_stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+const char *reload_js =
+    "<script>\n"
+    "const ws = new WebSocket(`ws://${window.location.hostname}:8082`);"
+
+    "ws.addEventListener('open', () => {"
+    "  console.log('ws connected');"
+    "  document.title = 'ws connected';"
+    "});"
+
+    "ws.addEventListener('message', (event) => {"
+    "  if (event.data === 'reload') {"
+    "    console.log('relod command received');"
+    "    location.reload();"
+    "  }"
+    "});"
+
+    "ws.addEventListener('error', (error) => {"
+    "  console.error('ws error:', error);"
+    "  document.title = 'ws connection error';"
+    "});"
+    "</script>\n";
 
 int main() {
   while (FCGI_Accept() >= 0) {
     printf("Content-Type: text/html\r\n\r\n");
-
-    printf("<h1>Hello, FastCGI!</h1>");
-    printf("<p>QUERY_STRING: %s</p>", getenv("QUERY_STRING"));
+    printf("<html><head><title>FastCGI</title>%s</head>", reload_js);
+    printf("<body><h1>Hello 9 FastCGI!</h1>");
+    printf("<p>Last update: %ld</p></body></html>", time(NULL));
   }
   return 0;
 }
