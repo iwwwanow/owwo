3470287031ac3e673b9d8ffec69a7f74122dc569
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-05-01 03:01:14 +0300

    chore(web): debug on main

--- web/src/main.c
+++ web/src/main.c
@@ -1,42 +1,61 @@
 #include <dirent.h>
-#include <fcgi_stdio.h>
+/* #include <fcgi_stdio.h> */
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "dev/dev.h"
+/* #include "dev/dev.h" */
+/* #include "ui/ui.h" */
 #include "getters/getters.h"
-#include "ui/ui.h"
+#include "models/resource.h"
+#include "models/resource_list.h"
+
+/* struct Resources *resources = get_filenames(path); */
+/* if (resources == NULL) { */
+/*   printf("resources is null"); */
+/*   return -1; */
+/* } */
 
 #define MAX_FILENAME_LENGTH 256
 
 int main() {
+  ResourceList resources;
+  ResourceList_init(&resources);
 
-  const char *reload_js = get_reload_js();
+  const char *path = "/data/uploads";
+
+  scan_directory(path, &resources);
+
+  for (size_t i = 0; i < resources.size; i++) {
+    printf("Path: %s\n", resources.data[i].path);
+    printf("MIME: %s\n", resources.data[i].mimetype);
+    printf("Name: %s\n", resources.data[i].name);
+    printf("Cover: %s\n", resources.data[i].cover);
+    printf("\n");
+  }
+
+  ResourceList_free(&resources);
+  return 0;
+
+  /* const char *reload_js = get_reload_js(); */
   /* TODO env & fallback */
-  const char *path = "/data/uploads/";
+  /* const char *path = "/data/uploads/"; */
 
-  struct Resources *resources = get_filenames(path);
-  if (resources == NULL) {
-    printf("resources is null");
-    return -1;
-  }
-
-  while (FCGI_Accept() >= 0) {
-    char *request_method = getenv("REQUEST_METHOD");
-    char *request_uri = getenv("REQUEST_URI");
-    char *path_info = getenv("PATH_INFO");
-    char *query_string = getenv("QUERY_STRING");
-    char *remote_addr = getenv("REMOTE_ADDR");
-
-    write_log_message("%s\n", request_method);
-    write_log_message("%s\n", request_uri);
-    write_log_message("%s\n", query_string);
-    write_log_message("%s\n", remote_addr);
-    write_log_message("\n\n", NULL);
-
-    printf("Content-Type: text/html\r\n\r\n");
-    render_resource_page(reload_js, resources);
-  }
+  /* while (FCGI_Accept() >= 0) { */
+  /*   char *request_method = getenv("REQUEST_METHOD"); */
+  /*   char *request_uri = getenv("REQUEST_URI"); */
+  /*   char *path_info = getenv("PATH_INFO"); */
+  /*   char *query_string = getenv("QUERY_STRING"); */
+  /*   char *remote_addr = getenv("REMOTE_ADDR"); */
+  /*  */
+  /*   write_log_message("%s\n", request_method); */
+  /*   write_log_message("%s\n", request_uri); */
+  /*   write_log_message("%s\n", query_string); */
+  /*   write_log_message("%s\n", remote_addr); */
+  /*   write_log_message("\n\n", NULL); */
+  /*  */
+  /*   printf("Content-Type: text/html\r\n\r\n"); */
+  /*   render_resource_page(reload_js, resources); */
+  /* } */
 
   return 0;
 }
