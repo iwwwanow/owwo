d26e6a97329700361e914f8565592a2c57d92463
Kirill Ivanov <ik@atls.team>
2025-07-28 13:36:27 +0300

    refactor(common): rm useless for go

--- Globals.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.module.css";
-
-declare type Exists<T> = Exclude<T, null | undefined>;
-
-declare interface Constructor<T> {
-  new (): T;
-}
-
-declare type Component<T> = (props: T) => JSX.Element;
